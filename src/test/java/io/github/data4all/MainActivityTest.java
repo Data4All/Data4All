fcbc56fa62965d33146c9756032eaeb4ed0cd05a
tbrose <tbrose@informatik.uni-bremen.de>
2014-11-05 22:12:41 +0100

    adding default testcase for testing jenkins

---
+++ src/test/java/io/github/data4all/MainActivityTest.java
@@ -0,0 +1,14 @@
+package io.github.data4all;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
