7832880c502246efa0becfc142de7234a12e6662
tbrose <tbrose@informatik.uni-bremen.de>
2014-11-05 22:14:33 +0100

    removed failing testcase

--- src/test/java/io/github/data4all/MainActivityTest.java
+++ src/test/java/io/github/data4all/MainActivityTest.java
@@ -1,14 +1,4 @@
 package io.github.data4all;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class MainActivityTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
 }
