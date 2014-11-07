c3ea8b4ee800d24c68496379899730a368148f32
Stefan Bollen <sbollen@informatik.uni-bremen.de>
2014-11-07 10:32:01 +0100

    "Pictures can be taken"

--- src/main/java/io/github/data4all/MainActivity.java
+++ src/main/java/io/github/data4all/MainActivity.java
@@ -1,11 +1,13 @@
 package io.github.data4all;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -19,22 +21,23 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        Button b = (Button) findViewById(R.id.buttonCamera);
+        b.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MakePhotoActivity.class));
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(io.github.data4all.R.menu.main, menu);
 	return true;
-    }
-    
-    /** Called when user clicks help button */
-    public void showHelpToast(View view){
-    	Toast toast;
-    	toast = Toast.makeText(getApplicationContext(), "Default help text.", Toast.LENGTH_SHORT);
-    	if(toast!=null){
-    		toast.show();
-    	}
     }
 
 }
---
+++ src/main/java/io/github/data4all/MakePhotoActivity.java
@@ -0,0 +1,94 @@
+package io.github.data4all;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+public class MakePhotoActivity extends Activity {
+    private final static String DEBUG_TAG = "MakePhotoActivity";
+    private Camera camera;
+    private int cameraId = 0;
+    private static final int CAMERA_REQUEST = 100;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makephoto);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+    }
+
+    public void onClick(View view) {
+
+        // do we have a camera?
+        if (!getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            cameraId = findFrontFacingCamera();
+            if (cameraId < 0) {
+                Toast.makeText(this, "No front facing camera found.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                camera = Camera.open(cameraId);
+            }
+        }
+
+        // create Intent to take a picture and return control to the calling
+        // application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode != RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            imageView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                Log.d(DEBUG_TAG, "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+}
