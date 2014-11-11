340ac64bbb0b245a392ec01da1a9e82d2fcf2e21
mboyke <mboyke@uni-bremen.de>
2014-11-11 17:33:23 +0100

    added Speech Recognition

--- src/main/java/io/github/data4all/MainActivity.java
+++ src/main/java/io/github/data4all/MainActivity.java
@@ -1,17 +1,27 @@
 package io.github.data4all;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	protected static final int REQUEST_CODE = 1234;
+	Button speech;
+	ArrayList<String> matches_text;
+	Dialog match_text_dialog;
     /**
      * Called when the activity is first created.
      * @param savedInstanceState If the activity is being re-initialized after 
@@ -30,9 +40,50 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, MakePhotoActivity.class));
             }
         });
+        
+        speech = (Button) findViewById(R.id.button6);
+        speech.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {           
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    startActivityForResult(intent, REQUEST_CODE);
+                
+            }
+        });
+     
     }
 
 
+	
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+     if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+     // dialog for search result
+     match_text_dialog = new Dialog(MainActivity.this);
+     match_text_dialog.setContentView(R.layout.dialog_matches);
+     match_text_dialog.setTitle("Select Matching Tag");
+     ListView textlist = (ListView)match_text_dialog.findViewById(R.id.list);
+     matches_text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+     ArrayAdapter<String> adapter =    new ArrayAdapter<String>(this,
+           android.R.layout.simple_list_item_1, matches_text);
+     textlist.setAdapter(adapter);
+     textlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+     // show the result 
+     @Override
+     public void onItemClick(AdapterView<?> parent, View view,
+                             int position, long id) {
+       speech.setText("You have said " +matches_text.get(position));
+       match_text_dialog.hide();
+     }
+     });
+     match_text_dialog.show();
+     }
+     super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 	// Inflate the menu; this adds items to the action bar if it is present.
