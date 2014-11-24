e696694e15cea5fc5205ccf6205ad37172e1b989
Timo Brose <tbrose@fb3.uni-bremen.de>
2014-11-24 09:23:12 +0100

    Reset Repository to initial state

--- src/main/java/io/github/businessmodel/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The classes in this package represent the xml structure of open street map data.
- * Basically it is a list of instances of our data primitives (nodes, ways, and relations) that are the architecture of the OSM model.
- * Detailed information about the osm xml structure can be found in the osm wiki:
- * http://wiki.openstreetmap.org/wiki/OSM_XML 
- * @author Felix Kirchgeorg  
- */
-package io.github.businessmodel;
--- src/main/java/io/github/data4all/MainActivity.java
+++ src/main/java/io/github/data4all/MainActivity.java
@@ -1,110 +1,30 @@
 package io.github.data4all;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity {	
 
-	protected static final int REQUEST_CODE = 1234;
-	Button speech;
-	Button camera;
-	Button editor;
-	ArrayList<String> matches_text;
-	Dialog match_text_dialog;
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        camera = (Button) findViewById(R.id.buttonCamera);
-        camera.setOnClickListener(this);
-        editor = (Button) findViewById(R.id.buttonEditor);
-        editor.setOnClickListener(this);        
-        speech = (Button) findViewById(R.id.button6);
-        speech.setOnClickListener(this);   
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-     
-    }
-
-    public void onClick(View v) {
-    	if(v == camera){
-            startActivity(new Intent(MainActivity.this, CameraActivity.class));
-    	}
-    	else if(v == editor){
-            startActivity(new Intent(MainActivity.this, EditorActivity.class));
-    	}
-    	else if(v == speech){
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            startActivityForResult(intent, REQUEST_CODE);
-    	}
-    }
-
-	
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-     if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-     // dialog for search result
-     match_text_dialog = new Dialog(MainActivity.this);
-     match_text_dialog.setContentView(R.layout.dialog_matches);
-     match_text_dialog.setTitle("Select Matching Tag");
-     ListView textlist = (ListView)match_text_dialog.findViewById(R.id.list);
-     matches_text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-     ArrayAdapter<String> adapter =    new ArrayAdapter<String>(this,
-           android.R.layout.simple_list_item_1, matches_text);
-     textlist.setAdapter(adapter);
-     textlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-     // show the result 
-     @Override
-     public void onItemClick(AdapterView<?> parent, View view,
-                             int position, long id) {
-       speech.setText("You have said " +matches_text.get(position));
-       match_text_dialog.hide();
-     }
-     });
-     match_text_dialog.show();
-     }
-     super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(io.github.data4all.R.menu.main, menu);
-	return true;
-    }
-    
-    /** Called when user clicks the help button. */
-    public void showHelpToast(View view){
-    	Toast toast;
-    	toast = Toast.makeText(getApplicationContext(), "Default help text.", Toast.LENGTH_SHORT);
-    	
-    	if(toast != null){
-    		toast.show();
-    	}
-    }
-
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
-
