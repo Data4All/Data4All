7fd83a6c347e634f6ceb2f6ff354425236a67c34
Timo Brose <tbrose@fb3.uni-bremen.de>
2014-11-27 00:09:32 +0100

    Add own implementation of a point refs #2582

---
+++ src/main/java/io/github/data4all/model/drawing/Point.java
@@ -0,0 +1,36 @@
+package io.github.data4all.model.drawing;
+
+/**
+ * A Point is a Object that can hold an x and y coordinate There is no further
+ * logic in this class
+ * 
+ * @author tbrose
+ */
+public class Point {
+	private final float x;
+	private final float y;
+
+	public Point(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return (o == this)
+				|| ((o instanceof Point) && x == ((Point) o).getX() && y == ((Point) o)
+						.getY());
+	}
+
+	public boolean equals(float x, float y) {
+		return this.x == y && this.y == y;
+	}
+}
