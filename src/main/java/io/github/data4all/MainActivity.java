adf385c64f2ff2980f015f5394a3abff57e4098c
Steeve <nkemson@tzi.de>
2014-11-27 12:05:11 +0100

    i have created a button button_sensor in the MainActivity

--- src/main/java/io/github/data4all/MainActivity.java
+++ src/main/java/io/github/data4all/MainActivity.java
@@ -1,11 +1,19 @@
 package io.github.data4all;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {	
-
+    
+	
+	
+	
+	Button button_sensor;
 	/**
 	 * Called when the activity is first created.
 	 * 
@@ -19,6 +27,16 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		 button_sensor = (Button) findViewById(R.id.buttonSensor);
+	     button_sensor.setOnClickListener((OnClickListener) this);   
+
+	}
+	
+	 public void onClick(View v) {
+	        if(v == button_sensor){
+	           startActivity(new Intent(MainActivity.this, LageSensor.class));
+	        }
 	}
 
 	@Override
