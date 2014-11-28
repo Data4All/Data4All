f26e27e6d0c791445d74bece5c222a3af8c3f738
vkochno <vkochno@tzi.de>
2014-11-28 12:37:39 +0100

    some small changes, better way for using image as backgrounds refs #2498

--- src/main/java/io/github/data4all/MainActivity.java
+++ src/main/java/io/github/data4all/MainActivity.java
@@ -7,19 +7,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {	
-	Button button;
-	ImageView image;
 	
-	private static final int SELECT_PICTURE = 1;
-
-    private String selectedImagePath;
-    private static int RESULT_LOAD_IMAGE = 1;
 
 	/**
 	 * Called when the activity is first created.
@@ -34,58 +29,8 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		((Button) findViewById(R.id.buttonLoadPicture))
-        .setOnClickListener(new OnClickListener() {
-        	@Override
-            public void onClick(View arg0) {
-            	System.out.println("testtest");
-            	Intent i = new Intent(
-            			Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            			 
-            			startActivityForResult(i, RESULT_LOAD_IMAGE);
-           
-            }
-        });
+		
 }
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-	      
-	     if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-	         Uri selectedImage = data.getData();
-	         String[] filePathColumn = { MediaStore.Images.Media.DATA };
-	 
-	         Cursor cursor = getContentResolver().query(selectedImage,
-	                 filePathColumn, null, null, null);
-	         cursor.moveToFirst();
-	 
-	         int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-	         String picturePath = cursor.getString(columnIndex);
-	         cursor.close();
-	     }
-    }
-
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-            // just some safety built in 
-            if( uri == null ) {
-                // TODO perform some logging or show user feedback
-                return null;
-            }
-            // try to retrieve the image from the media store first
-            // this will only work for images selected from gallery
-            String[] projection = { MediaStore.Images.Media.DATA };
-            Cursor cursor = managedQuery(uri, projection, null, null, null);
-            if( cursor != null ){
-                int column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                return cursor.getString(column_index);
-            }
-            // this is our fallback here
-            return uri.getPath();
-    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,4 +38,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	 @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch (item.getItemId()) {
+	        case R.id.ShowPicture:
+	            Intent intent = new Intent(this, ShowPictureActivity.class);
+	            startActivity(intent);
+	            return true;
+	        default:
+	            break;
+	        }
+	        return super.onOptionsItemSelected(item);
+	    }
 }
--- src/main/java/io/github/data4all/ShowPictureActivity.java
+++ src/main/java/io/github/data4all/ShowPictureActivity.java
@@ -1,50 +1,73 @@
 package io.github.data4all;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class ShowPictureActivity extends Activity {
-	
- 
+	Button button;
+	ImageView image;
+	Bitmap bmp;
 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_picture);
+		((Button) findViewById(R.id.btnGallary))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View arg0) {
+						openGallery();
+					}
+				});
+	}
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        System.out.println("testtest");
-        setContentView(R.layout.activity_picture);
-    }
-        
+	private void openGallery() {
+		Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+		photoPickerIntent.setType("image/*");
+		startActivityForResult(photoPickerIntent, 1);
+	}
 
-   
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-            // just some safety built in 
-            if( uri == null ) {
-                // TODO perform some logging or show user feedback
-                return null;
-            }
-            // try to retrieve the image from the media store first
-            // this will only work for images selected from gallery
-            String[] projection = { MediaStore.Images.Media.DATA };
-            Cursor cursor = managedQuery(uri, projection, null, null, null);
-            if( cursor != null ){
-                int column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                return cursor.getString(column_index);
-            }
-            // this is our fallback here
-            return uri.getPath();
-    }
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
 
+		if (requestCode == 1) {
+			if (data != null && resultCode == RESULT_OK) {
+
+				Uri selectedImage = data.getData();
+
+				String[] filePathColumn = { MediaStore.Images.Media.DATA };
+				Cursor cursor = getContentResolver().query(selectedImage,
+						filePathColumn, null, null, null);
+				cursor.moveToFirst();
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				String filePath = cursor.getString(columnIndex);
+				cursor.close();
+
+				if (bmp != null && !bmp.isRecycled()) {
+					bmp = null;
+				}
+
+				bmp = BitmapFactory.decodeFile(filePath);
+				image.setBackgroundResource(0);
+				image.setImageBitmap(bmp);
+			} else {
+				Log.d("Status:", "Photopicker canceled");
+			}
+		}
+	}
 }
