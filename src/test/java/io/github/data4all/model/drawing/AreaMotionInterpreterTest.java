aa1c6b20c61bbdb2298c1d3d7e9836b74667d2f4
Timo Brose <tbrose@fb3.uni-bremen.de>
2014-12-03 09:42:34 +0100

    Finished tests for AreaMotionInterpreter refs #2854

--- src/test/java/io/github/data4all/model/drawing/AreaMotionInterpreterTest.java
+++ src/test/java/io/github/data4all/model/drawing/AreaMotionInterpreterTest.java
@@ -3,6 +3,7 @@ package io.github.data4all.model.drawing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Before;
@@ -23,10 +24,13 @@ import android.graphics.Canvas;
 @Config(emulateSdk = 18)
 public class AreaMotionInterpreterTest extends MotionInterpreterTest {
 
+    /**
+     * The interpreter to test
+     */
     private AreaMotionInterpreter interpreter;
 
     /**
-     * A mocked canvas to verify, if the right points and lines where drawed
+     * A mocked canvas to verify, if the right points and lines where drawn
      */
     private Canvas canvas;
 
@@ -37,6 +41,19 @@ public class AreaMotionInterpreterTest extends MotionInterpreterTest {
     }
 
     /**
+     * If the User is not typing any point, there should be nothing drawn
+     */
+    @Test
+    public void draw_zeroPoints_nothingIsDrawn() {
+        interpreter.draw(canvas, new ArrayList<DrawingMotion>());
+
+        verifyDrawCircle(canvas, times(0));
+        verifyDrawLine(canvas, times(0));
+    }
+
+    // Tests for points
+
+    /**
      * If the User is typing one point, there should be one point drawn
      */
     @Test
@@ -44,7 +61,7 @@ public class AreaMotionInterpreterTest extends MotionInterpreterTest {
         DrawingMotion motion = getDrawingMotion(0, 0);
         interpreter.draw(canvas, Arrays.asList(motion));
 
-        verifyDrawCircle(canvas, times(1));
+        verifyDrawCircle(canvas, times(1), 0, 0);
     }
 
     /**
@@ -56,11 +73,12 @@ public class AreaMotionInterpreterTest extends MotionInterpreterTest {
         DrawingMotion motion2 = getDrawingMotion(20, 20);
         interpreter.draw(canvas, Arrays.asList(motion1, motion2));
 
-        verifyDrawCircle(canvas, times(2));
+        verifyDrawCircle(canvas, times(1), 0, 0);
+        verifyDrawCircle(canvas, times(1), 20, 20);
     }
 
     /**
-     * If the User is typing two points tear to each other, there should be two
+     * If the User is typing two points near to each other, there should be two
      * points drawn, because a line always have a start and a end point
      */
     @Test
@@ -69,18 +87,159 @@ public class AreaMotionInterpreterTest extends MotionInterpreterTest {
         DrawingMotion motion2 = getDrawingMotion(1, 0);
         interpreter.draw(canvas, Arrays.asList(motion1, motion2));
 
-        verifyDrawCircle(canvas, times(2));
+        verifyDrawCircle(canvas, times(1), 0, 0);
+        verifyDrawCircle(canvas, times(1), 1, 0);
     }
 
     /**
-     * If the User is typing a line which is a dot, there should be one
-     * point drawn, because a dot is only point
+     * If the User is typing a line which is a dot, there should be one point
+     * drawn, because a dot is only point
      */
     @Test
     public void draw_twoNearPointsInSamePath_onePointIsDrawn() {
         DrawingMotion motion = getDrawingMotion(0, 0, 1, 0);
         interpreter.draw(canvas, Arrays.asList(motion));
 
-        verifyDrawCircle(canvas, times(1));
+        verifyDrawCircle(canvas, times(1), 0.5f, 0);
+    }
+
+    /**
+     * If the User is typing a straight line, there should be two points drawn,
+     * because the line only needs a start and a end point
+     */
+    @Test
+    public void draw_straightLine_twoPointsWereDrawn() {
+        DrawingMotion motion = getDrawingMotion(0, 0, 10, 0, 20, 0, 30, 0, 40,
+                0, 50, 0, 60, 0, 70, 0, 80, 0, 90, 0);
+        interpreter.draw(canvas, Arrays.asList(motion));
+
+        verifyDrawCircle(canvas, times(1), 0, 0);
+        verifyDrawCircle(canvas, times(1), 90, 0);
+    }
+
+    /**
+     * If the User is typing a approximated line, there should be two points
+     * drawn, because the line only needs a start and a end point
+     */
+    @Test
+    public void draw_approximatedLine_twoPointsWereDrawn() {
+        DrawingMotion motion = getDrawingMotion(0, 0, 10, 1, 20, 0, 30, -1, 40,
+                0, 50, 1, 60, 0, 70, -1, 80, 0);
+        interpreter.draw(canvas, Arrays.asList(motion));
+
+        verifyDrawCircle(canvas, times(2));
+    }
+
+    /**
+     * If the User is typing a straight triangle, there should be three points
+     * drawn.
+     */
+    @Test
+    public void draw_straightTriangle_threePointsWereDrawn() {
+        // Try with three gestures
+        DrawingMotion motion1 = getDrawingMotion(0, 0, 10, 0, 20, 0, 30, 0, 40,
+                0);
+        DrawingMotion motion2 = getDrawingMotion(50, 0, 50, 10, 50, 20, 50, 30,
+                50, 40);
+        DrawingMotion motion3 = getDrawingMotion(50, 50, 40, 40, 30, 30, 20,
+                20, 10, 10);
+        interpreter.draw(canvas, Arrays.asList(motion1, motion2, motion3));
+        verifyDrawCircle(canvas, times(3));
+
+        // Try with one gestures, three more invocations should happen
+        DrawingMotion motion = getDrawingMotion(0, 0, 10, 0, 20, 0, 30, 0, 40,
+                0, 50, 0, 50, 10, 50, 20, 50, 30, 50, 40, 50, 50, 40, 40, 30,
+                30, 20, 20, 10, 10);
+        interpreter.draw(canvas, Arrays.asList(motion));
+        verifyDrawCircle(canvas, times(6));
+    }
+
+    // Tests for lines
+
+    /**
+     * If the User is typing one point, there should be one line with the length
+     * of 0 drawn
+     */
+    @Test
+    public void draw_onePoint_oneLineIsDrawn() {
+        DrawingMotion motion = getDrawingMotion(0, 0);
+        interpreter.draw(canvas, Arrays.asList(motion));
+
+        verifyDrawLine(canvas, times(1));
+    }
+
+    /**
+     * If the User is typing two points, there should be two lines drawn<br/>
+     * A line from point one to two and one line from point two to one
+     */
+    @Test
+    public void draw_twoPoints_twoLinesWereDrawn() {
+        DrawingMotion motion1 = getDrawingMotion(0, 0);
+        DrawingMotion motion2 = getDrawingMotion(20, 20);
+        interpreter.draw(canvas, Arrays.asList(motion1, motion2));
+
+        verifyDrawLine(canvas, times(2));
+    }
+
+    /**
+     * If the User is typing a line which is a dot, there should be one lines
+     * drawn, because a dot is only point
+     */
+    @Test
+    public void draw_twoNearPointsInSamePath_zeroLinesWereDrawn() {
+        DrawingMotion motion = getDrawingMotion(0, 0, 1, 0);
+        interpreter.draw(canvas, Arrays.asList(motion));
+
+        verifyDrawLine(canvas, times(1));
+    }
+
+    /**
+     * If the User is typing a straight line, there should be two lines drawn<br/>
+     * A line from point one to two and one line from point two to one
+     */
+    @Test
+    public void draw_straightLine_twoLinesWereDrawn() {
+        DrawingMotion motion = getDrawingMotion(0, 0, 10, 0, 20, 0, 30, 0, 40,
+                0, 50, 0, 60, 0, 70, 0, 80, 0, 90, 0);
+        interpreter.draw(canvas, Arrays.asList(motion));
+
+        verifyDrawLine(canvas, times(2));
+    }
+
+    /**
+     * If the User is typing a approximated line, there should be two lines drawn<br/>
+     * A line from point one to two and one line from point two to one
+     */
+    @Test
+    public void draw_approximatedLine_twoLinesWereDrawn() {
+        DrawingMotion motion = getDrawingMotion(0, 0, 10, 1, 20, 0, 30, -1, 40,
+                0, 50, 1, 60, 0, 70, -1, 80, 0);
+        interpreter.draw(canvas, Arrays.asList(motion));
+
+        verifyDrawLine(canvas, times(2));
+    }
+
+    /**
+     * If the User is typing a straight triangle, there should be three lines
+     * drawn.
+     */
+    @Test
+    public void draw_straightTriangle_threeLinesWereDrawn() {
+        // Try with three gestures
+        DrawingMotion motion1 = getDrawingMotion(0, 0, 10, 0, 20, 0, 30, 0, 40,
+                0);
+        DrawingMotion motion2 = getDrawingMotion(50, 0, 50, 10, 50, 20, 50, 30,
+                50, 40);
+        DrawingMotion motion3 = getDrawingMotion(50, 50, 40, 40, 30, 30, 20,
+                20, 10, 10);
+        interpreter.draw(canvas, Arrays.asList(motion1, motion2, motion3));
+        verifyDrawLine(canvas, times(3));
+
+        // Try with one gestures, three more invocations should happen
+        DrawingMotion motion = getDrawingMotion(0, 0, 10, 0, 20, 0, 30, 0, 40,
+                0, 50, 0, 50, 10, 50, 20, 50, 30, 50, 40, 50, 50, 40, 40, 30,
+                30, 20, 20, 10, 10);
+        interpreter.draw(canvas, Arrays.asList(motion));
+        verifyDrawLine(canvas, times(6));
     }
 }
