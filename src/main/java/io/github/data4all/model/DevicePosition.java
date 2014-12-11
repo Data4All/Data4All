09d11815fef6c97a9994ec772d59e264d70a3312
Stefan Bollen <sbollen@informatik.uni-bremen.de>
2014-12-11 14:18:23 +0100

    Added new Model for the device postion and a class for recording and optimizing gps and sensor data.

---
+++ src/main/java/io/github/data4all/model/DevicePosition.java
@@ -0,0 +1,70 @@
+package io.github.data4all.model;
+
+import io.github.data4all.model.drawing.Point;
+
+import java.util.Calendar;
+
+import android.location.Location;
+
+/**
+ * A model for the postion of the device. It includes the location and the
+ * rotation of the device.
+ * 
+ * @author sbollen
+ *
+ */
+
+public class DevicePosition {
+
+    // Location of the device (GPS)
+    Location location;
+
+    // ACCELEROMETER Sensor Values
+    private float[] accelValues = new float[3];
+    // MAGNETIC_FIELD Sensor Values
+    private float[] magValues = new float[3];
+
+    // Actual time when the data was recorded
+    private long timestamp;
+
+    public DevicePosition(Location location, float[] accelValues,
+            float[] magValues, long timestamp) {
+        this.location = location;
+        this.accelValues = accelValues;
+        this.magValues = magValues;
+        this.timestamp = timestamp;
+
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public float[] getAccelValues() {
+        return accelValues;
+    }
+
+    public void setAccelValues(float[] accelValues) {
+        this.accelValues = accelValues;
+    }
+
+    public float[] getMagValues() {
+        return magValues;
+    }
+
+    public void setMagValues(float[] magValues) {
+        this.magValues = magValues;
+    }
+
+    public boolean equals(Object o) {
+        return this == o
+                || ((o instanceof DevicePosition)
+                        && location == ((DevicePosition) o).getLocation()
+                        && accelValues == ((DevicePosition) o).getAccelValues()
+                        && magValues == ((DevicePosition) o).getMagValues());
+    }
+}
---
+++ src/main/java/io/github/data4all/util/DevicePositionUtils.java
@@ -0,0 +1,27 @@
+package io.github.data4all.util;
+
+/**
+ * Record the GPS and sensor data of the device and optimize them.
+ * 
+ * @author sbollen
+ *
+ */
+
+public class DevicePositionUtils {
+
+    public DevicePositionUtils() {
+        // TODO Auto-generated constructor stub
+    }
+
+    // A method for getting the actual location
+
+    // A method for getting the actual sensor data
+
+    // A method for setting up the ringbuffer and filling it with the models
+    // DevicePosition
+
+    // A method for optimizing the location data of the ringbuffer
+
+    // A method for optimizing the sensor data of the ringbuffer
+
+}
