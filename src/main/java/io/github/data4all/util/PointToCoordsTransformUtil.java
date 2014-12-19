befcab78ee40d0f77607f54be2847678842fa59f
Andreas Burghardt <andi@fb3.uni-bremen.de>
2014-12-19 10:19:43 +0100

    Added some Logs

--- src/main/java/io/github/data4all/util/PointToCoordsTransformUtil.java
+++ src/main/java/io/github/data4all/util/PointToCoordsTransformUtil.java
@@ -1,12 +1,19 @@
 package io.github.data4all.util;
 
-import java.util.ArrayList;
+/**
+ * This Class uses the Orientation of the Phone, the Pixel 
+ * and the Camera-Parameters to calculate the Distance between 
+ * the Phone and the Object in a 2D System.
+ * 
+ * @author AndreasBurghardt
+ * @version 0.1
+ *
+ */
+
 
 import io.github.data4all.logger.*;
-
 import java.lang.Math;
 
-import toxi.geom.*;
 
 public class PointToCoordsTransformUtil {
 	static String TAG = "PointToWorldCoords";
@@ -17,21 +24,36 @@ public class PointToCoordsTransformUtil {
 	public float[] calculate (float[] o){
 		this.orientation = o;
 		
-		float z = (float) Math.cos(orientation[1]); //calculate Z with Pitch
-		float yy = (float) Math.sin(-orientation[1]); //calculate temp.Y with Pitch
-		float xx = (float) (Math.tan(orientation[2]) * z);//calculate temp.X with fix Z and Roll
+		Log.d(TAG,"Delivered Phoneorientation: azimuth = " 
+				+ o[0] +" ,pitch = " + o[1] + ", roll = " + o[2]);
+		
+		//calculate Z with Pitch
+		float z = (float) Math.cos(orientation[1]); 
+		
+		//calculate temp.Y with Pitch
+		float yy = (float) Math.sin(-orientation[1]); 
+		
+		//calculate temp.X with fix Z and Roll
+		float xx = (float) (Math.tan(orientation[2]) * z);
+		
 		// Rotate Vector with Azimuth
 		float x = (float) ((xx * Math.cos(orientation[0])) - (yy * Math.sin(orientation[0])));
 		float y = (float) ((xx * Math.sin(orientation[0])) + (yy * Math.cos(orientation[0])));
 		
-		z = height / z;
+		if(z >= 0){
+			z=-1;
+			Log.d(TAG,"Camera is looking to the sky.");
+		}
+		
+		z = height / (-z);
 		x = x * z;
 		y = y * z;
 		coords[0] = x;
 		coords[1] = y;
+		
+		Log.d(TAG,"Calculated x = " + x + " and y = " + y);
 		return coords;
 		
 	}
-	
-	
+		
 }
