492eac3abda3c607d4fdb3fc86c0aa1ade3f7ee0
Felix Kirchgeorg <fkirchge@informatik.uni-bremen.de>
2015-01-16 16:55:30 +0100

    transformationparambean implements parcelable and tested refs #2966

--- src/main/java/io/github/data4all/model/data/TransformationParamBean.java
+++ src/main/java/io/github/data4all/model/data/TransformationParamBean.java
@@ -1,12 +1,12 @@
 package io.github.data4all.model.data;
 
-import java.util.ArrayList;
-
 import android.location.Location;
-import io.github.data4all.model.data.GPSBean;
-import io.github.data4all.model.drawing.Point;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class TransformationParamBean {
+
+public class TransformationParamBean implements Parcelable {
+	
 	private double height;
 	private double cameraMaxRotationAngle;
 	private double cameraMaxPitchAngle;
@@ -22,11 +22,7 @@ public class TransformationParamBean {
 		this.photoHeight = photoHeight;
 		this.photoWidth = photoWidth;
 		this.location = location;
-
 	}
-	
-	
-	
 	
 	public double getHeight() {
 		return height;
@@ -69,20 +65,69 @@ public class TransformationParamBean {
 	}
 
 
-
-
-
 	public Location getLocation() {
 		return location;
 	}
-
-
-
 
 	public void setLocation(Location location) {
 		this.location = location;
 	}
 
+    /**
+     * Methods to write and restore a Parcel.
+     */
+    public static final Parcelable.Creator<TransformationParamBean> CREATOR
+            = new Parcelable.Creator<TransformationParamBean>() {
+    	
+        public TransformationParamBean createFromParcel(Parcel in) {
+            return new TransformationParamBean(in);
+        }
 
+        public TransformationParamBean[] newArray(int size) {
+            return new TransformationParamBean[size];
+        }
+    };
+    
+    
+    public int describeContents() {
+		return 0;
+	}
+    
+    /**
+     * Writes the nodes to the given parcel.
+     */
+	public void writeToParcel(Parcel dest, int flags) {		
+		dest.writeDouble(height);
+		dest.writeDouble(cameraMaxRotationAngle);
+		dest.writeDouble(cameraMaxPitchAngle);
+		dest.writeInt(photoWidth);
+		dest.writeInt(photoHeight);
+		if (location != null) {
+			dest.writeInt(1);
+			dest.writeString(location.getProvider());
+			dest.writeDouble(location.getLatitude());
+			dest.writeDouble(location.getLongitude());
+		} else {
+			dest.writeInt(0);
+		}
+	}
 
+	/**
+	 * Constructor to create a transformationbean from a parcel.
+	 * @param in
+	 */
+    private TransformationParamBean(Parcel in) {
+    	height = in.readDouble();
+    	cameraMaxRotationAngle = in.readDouble();
+    	cameraMaxPitchAngle = in.readDouble();
+    	photoWidth = in.readInt();
+    	photoHeight = in.readInt();
+    	if (in.readInt() != 0) {
+    		location = new Location(in.readString());
+    		location.setLatitude(in.readDouble());
+    		location.setLongitude(in.readDouble());
+    	}
+    }	
+
+	
 }
