85c15749af7507410e954b4714984c60ed6287ea
Steeve <nkemson@tzi.de>
2015-01-16 13:50:43 +0100

    created a new activity which get the reaction time of sensors when user touching the screen

--- src/main/java/io/github/data4all/activity/SensorReactionActivity.java
+++ src/main/java/io/github/data4all/activity/SensorReactionActivity.java
@@ -2,86 +2,61 @@ package io.github.data4all.activity;
 
 import io.github.data4all.R;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.hardware.SensorEvent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
 @SuppressLint("ClickableViewAccessibility")
-public class SensorReactionActivity extends SensorActivity implements
+public class SensorReactionActivity extends Activity implements
 		OnTouchListener {
-
-	// examine if the User touch the screen
-	boolean isTouched;
 
 	// variable for storing the time of first click
 	long startTime;
 
-	// time,when the current action started
-	long touchInitialTime;
-
-	// store how many time i pressed the screen
-	long duration;
-
-	// sensor reference time
-	long sensorReferenceTime;
-	
-	
-	//delay time for sensor
+	// delay time for sensor
 	long reactionTime;
-	
-	private static final float NS2S = 1.0f / 1000000000.0f;
 
-	private static final String TAG = "SensorReactionActivity";
+	private float[] lastValues;
+
+	private static final double JUMP_VALUE = 2;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sensor);
+	}
 
-		// i suppose the user don't touch the screen, when this activity is
-		// starting
-		isTouched = false;
-	}
-    
-	public void onSensorChanged(SensorEvent event){
-		if(sensorReferenceTime ==0){
-			sensorReferenceTime = event.timestamp;
+	public void onSensorChanged(SensorEvent event) {
+		lastValues = event.values;
+		if (Math.sqrt(Math.pow(event.values[0] - lastValues[0], 2)
+				+ Math.pow(event.values[1] - lastValues[1], 2)
+				+ Math.pow(event.values[2] - lastValues[2], 2)) > JUMP_VALUE) {
+			// Jump detected .....
+			long millis = System.currentTimeMillis();
+			reactionTime = millis - startTime;
+
+			new AlertDialog.Builder(this).setTitle("Delay:")
+					.setMessage("" + reactionTime)
+					.setPositiveButton("Okay", null).show();
 		}
-		else{
-			 sensorReferenceTime = System.currentTimeMillis() + (long) ((event.timestamp - sensorReferenceTime) * NS2S);    
-		}
+		
 	}
-	
+
 	/**
-	 * when the user touch the screen then store the initial Time on the variable startTime
-	 * and the time when the user have touching the screen on the variable touchInitialTime
+	 * when the user touch the screen then store the initial Time on the
+	 * variable startTime and the time when the user have touching the screen on
+	 * the variable touchInitialTime
 	 * 
 	 */
-	
+
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_HOVER_MOVE) {
-			// show that i have touched the screen
-			isTouched = true;
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
 			startTime = System.currentTimeMillis();
-			touchInitialTime = event.getDownTime();
-
-            if(sensorReferenceTime >= touchInitialTime){
-            	reactionTime = sensorReferenceTime - touchInitialTime;
-            }	
-            
-			Log.d(TAG, "reaction time " + reactionTime);
-
-		}  else if (event.getAction() == MotionEvent.ACTION_UP) {
-			isTouched = false;
-			long time = System.currentTimeMillis() - touchInitialTime;
-			duration = duration + time;
-
-			Log.d(TAG, "total time " + duration);
 		}
-
 		return super.onTouchEvent(event);
 	}
 
