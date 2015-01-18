d149eabaa9be8f59842b934b2b564c75e54e2284
8myth8 <rrohde@informatik.uni-bremen.de>
2015-01-18 15:04:34 +0100

    refs # 2559 refs #2576 worked on Parser a little more

--- src/main/java/io/github/data4all/util/OsmChangeParser.java
+++ src/main/java/io/github/data4all/util/OsmChangeParser.java
@@ -1,27 +1,60 @@
 package io.github.data4all.util;
 
+import android.annotation.SuppressLint;
 import io.github.data4all.logger.Log;
 import io.github.data4all.model.data.Node;
+import io.github.data4all.model.data.OsmElement;
 import io.github.data4all.model.data.Relation;
 import io.github.data4all.model.data.RelationMember;
 import io.github.data4all.model.data.Way;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.SortedMap;
 
+@SuppressLint("SimpleDateFormat")
 public class OsmChangeParser {
 
 	
-	public static void parse(String filename){
+	public static void parse(String filename, List<OsmElement> elems, long changesetID){
 		try {
 			PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
+			ArrayList<Node> nodes = new ArrayList<Node>();
+			ArrayList<Way> ways = new ArrayList<Way>();
+			ArrayList<Relation> relations = new ArrayList<Relation>();
+			ArrayList<RelationMember> relationMembers = new ArrayList<RelationMember>();
 			
+			for(OsmElement osm :elems){
+				if(osm.getClass().isInstance(Node.class)){
+					nodes.add((Node) osm);
+				}
+				if(osm.getClass().isInstance(Way.class)){
+					Way way =(Way) osm;
+					nodes.addAll(way.getNodes());
+					ways.add(way);
+				}
+				if(osm.getClass().isInstance(Relation.class)){
+					Relation rel =(Relation) osm;
+					rel.getMembers();
+					//TODO: RelationMembers hinzufügen benötigt DataBaseHandler
+					relations.add(rel);
+				}
+			}
 			
+			for (Node n: nodes){
+				parseNode(writer,n,changesetID);
+			}
+			for (Way w: ways){
+				parseWay(writer,w,changesetID);
+			}
+			for (Relation r: relations){
+				parseRelation(writer,r,changesetID);
+			}
 			
 			writer.flush();
 			Log.i("OsmChangeParser", "Data is flushed");
@@ -34,7 +67,7 @@ public class OsmChangeParser {
 		}
 	}
 	
-	private void parseNode(PrintWriter writer, Node node, long changesetID){
+	private static void parseNode(PrintWriter writer, Node node, long changesetID){
 		SimpleDateFormat dateformat = new SimpleDateFormat(
 				"yyyy-MM-dd'T'HH:mm:ss.SXXX");
 		writer.print("<node id=\""+ node.getOsmId() +"\" timestamp=\""+ dateformat.format(new Date()) 
@@ -54,7 +87,7 @@ public class OsmChangeParser {
 		writer.println("</node>");
 	}
 	
-	private void parseWay(PrintWriter writer, Way way, long changesetID){
+	private static void parseWay(PrintWriter writer, Way way, long changesetID){
 		SimpleDateFormat dateformat = new SimpleDateFormat(
 				"yyyy-MM-dd'T'HH:mm:ss.SXXX");
 		writer.println("<way id=\""+ way.getOsmId() +"\" timestamp=\""+ dateformat.format(new Date()) 
@@ -70,7 +103,7 @@ public class OsmChangeParser {
 		writer.println("</way>");	
 	}
 	
-	private void parseRelation(PrintWriter writer, Relation relation,long changesetID){
+	private static void parseRelation(PrintWriter writer, Relation relation,long changesetID){
 		SimpleDateFormat dateformat = new SimpleDateFormat(
 				"yyyy-MM-dd'T'HH:mm:ss.SXXX");
 		writer.println("<relation id=\""+ relation.getOsmId() +"\" timestamp=\""+ dateformat.format(new Date()) 
