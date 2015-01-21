05e8bb047b814d53b1042835b1c0db2a09096c5d
Steeve <nkemson@tzi.de>
2015-01-22 00:46:54 +0100

    #2544 added more Tests for SensorReactionActivity

--- src/test/java/io/github/data4all/activity/SensorReactionActivityTest.java
+++ src/test/java/io/github/data4all/activity/SensorReactionActivityTest.java
@@ -1,6 +1,10 @@
 package io.github.data4all.activity;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import io.github.data4all.logger.Log;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,17 +13,22 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import android.content.Intent;
+import android.view.MotionEvent;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
 public class SensorReactionActivityTest {
 
-	SensorReactionActivity sensorReactionActivity;
-	
-	
-    @Before
+	private SensorReactionActivity sensorReactionActivity;
+	private boolean mActivityStarted = false;
+	private boolean mActivityStopped = false;
+	private boolean isTouched;
+	private MotionEvent event;
+
+	@Before
 	public void setUp() throws Exception {
-		
+
 		this.sensorReactionActivity = Robolectric
 				.buildActivity(SensorReactionActivity.class).create().get();
 	}
@@ -28,10 +37,58 @@ public class SensorReactionActivityTest {
 	public void checkActivityNotNull() throws Exception {
 		assertNotNull(sensorReactionActivity);
 	}
-	
-	
 
-	
+	/**
+	 * check if Activity was started
+	 * @throws Exception
+	 */
+	@Test
+	public void checkActivityStarted() throws Exception {
+		checkActivityNotNull();
+		if (mActivityStarted) {
+			Intent startIntent = new Intent(Robolectric.application,
+					SensorReactionActivity.class);
+			sensorReactionActivity.startActivity(startIntent);
+			mActivityStarted = true;
+			assertTrue(mActivityStarted);
+		}
 
-	
+	}
+
+	/**
+	 * check if Activity was stopped
+	 * @throws Exception
+	 */
+	@Test
+	public void checkActivityStopped() throws Exception {
+		checkActivityStarted();
+		if (mActivityStarted || mActivityStopped) {
+			sensorReactionActivity.onStop();
+			mActivityStopped = true;
+		}
+
+	}
+    
+	/**
+	 * check if Screen has been touching
+	 * @throws Exception
+	 */
+	@Test
+	public void checkScreenTouched() throws Exception {
+		checkActivityStarted();
+		if (sensorReactionActivity.onTouchEvent(event)) {
+			isTouched = true;
+		}
+	}
+
+	/**
+	 * get Time, when screen has been touching
+	 * @throws Exception
+	 */
+	@Test
+	public void getTimeWhenScreenBeenTouched() throws Exception {
+		checkScreenTouched();
+		long startTime = System.currentTimeMillis();
+		assertEquals(System.currentTimeMillis(), startTime);
+	}
 }
