a5d072e0cd6cd03335ed0c33aca43d8ffa22d27b
Timo Brose <tbrose@fb3.uni-bremen.de>
2015-01-21 10:57:53 +0100

    Restructuring test cases refs #2856

--- src/test/java/io/github/data4all/view/TouchViewTest.java
+++ src/test/java/io/github/data4all/view/TouchViewTest.java
@@ -1,12 +1,14 @@
 package io.github.data4all.view;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import io.github.data4all.model.drawing.Point;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-
-import io.github.data4all.model.drawing.Point;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,15 +26,13 @@ import org.robolectric.annotation.Config;
 @Config(emulateSdk = 18)
 public class TouchViewTest {
 
-    TouchView touchview;
-    List<Point> polygon;
-    Point point1;
-    Point point2;
-    Point point3;
-
-    Point point4;
-    Point point5;
-    Point point6;
+    private TouchView touchview;
+    private List<Point> polygon;
+    
+    private Point point1;
+    private Point point2;
+    private Point point3;
+    private Point point4;
 
     @Before
     public void setUp() throws Exception {
@@ -42,10 +42,12 @@ public class TouchViewTest {
         point2 = new Point(40, 40);
         point3 = new Point(43, 43);
         point4 = new Point(33, 33);
+
         Field declaredField = touchview.getClass().getDeclaredField("polygon");
         declaredField.setAccessible(true);
         polygon = new ArrayList<Point>();
         declaredField.set(touchview, polygon);
+
         polygon.add(point1);
         polygon.add(point2);
         polygon.add(point3);
@@ -53,31 +55,56 @@ public class TouchViewTest {
     }
 
     @Test
-    public void testDeletePoint() {
-        // point5 isnt in the polygon
-        touchview.deletePoint(point5);
-        assertEquals(false, polygon.contains(point5));
-
-        // first check if point 4 is in the polygon and check afterwards
-        assertTrue(polygon.contains(point4));
-        touchview.deletePoint(point4);
-        assertEquals(false, polygon.contains(point4));
-
+    public void deletePoint_deleteNull_nothingChanged() {
+        List<Point> list = new ArrayList<Point>(polygon);
+        touchview.deletePoint(null);
+        for(int i = 0; i < list.size(); i++) {
+            assertEquals(list.get(i), polygon.get(i));
+        }
     }
 
     @Test
-    public void testLookUp() {
-        // there is no point around
-        assertNull(touchview.lookUp(20, 20, 2));
-        // there is a point near x=1 and y=1 -->point1
-        assertEquals(point1, touchview.lookUp(1, 1, 5));
-        // there is no point within the maximum distance. point 1 is too far
-        // away
-        assertNull(touchview.lookUp(5, 5, 2));
-        // we hit exactly a point
-        assertEquals(point1, touchview.lookUp(2, 2, 0));
-        // there are two points around (point 2 and 3)
-        assertEquals(point3, touchview.lookUp(42, 42, 2));
+    public void deletePoint_pointNotInPolygon_nothingChanged() {
+        List<Point> list = new ArrayList<Point>(polygon);
+        Point point5 = new Point(333, 333);
+        touchview.deletePoint(point5);
 
+        assertFalse(polygon.contains(point5));
+        for(int i = 0; i < list.size(); i++) {
+            assertEquals(list.get(i), polygon.get(i));
+        }
+    }
+
+    @Test
+    public void deletePoint_pointInPolygon_pointRemoved() {
+        assertTrue(polygon.contains(point4));
+        touchview.deletePoint(point4);
+        assertFalse(polygon.contains(point4));
+    }
+
+    @Test
+    public void lookUp_noPointsInPolygon_nullReturned() {
+        polygon.clear();
+        assertNull(touchview.lookUp(5, 5, 2));
+    }
+
+    @Test
+    public void lookUp_noPointAround_nullReturned() {
+        assertNull(touchview.lookUp(20, 20, 2));
+    }
+
+    @Test
+    public void lookUp_pointAround_rightPointReturned() {
+        assertEquals(point1, touchview.lookUp(1, 1, 5));
+    }
+
+    @Test
+    public void lookUp_exactLookupOnPoint_rightPointReturned() {
+        assertEquals(point1, touchview.lookUp(2, 2, 0));
+    }
+
+    @Test
+    public void lookUp_towPointsInRange_nearestPointReturned() {
+        assertEquals(point3, touchview.lookUp(42, 42, 2));
     }
 }
