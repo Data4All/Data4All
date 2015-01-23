a14a82ab93eef2c07fa89a6e06023f36f62c41cd
Steeve <nkemson@tzi.de>
2015-01-23 12:53:59 +0100

    ref #2544 added a new Array which take the filteredValues.

--- src/main/java/io/github/data4all/smoothing/BasicSmoothingSensorMethod.java
+++ src/main/java/io/github/data4all/smoothing/BasicSmoothingSensorMethod.java
@@ -8,27 +8,29 @@ package io.github.data4all.smoothing;
  */
 public class BasicSmoothingSensorMethod implements SensorSmoother {
 	/*
-	 * time smoothing constant for low-pass filter
-	 * for more smoothing is it important that APHA been between 0 and 1 ;
-	 * @See: http://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
+	 * time smoothing constant for low-pass filter for more smoothing is it
+	 * important that APHA been between 0 and 1 ;
+	 * 
+	 * @See:
+	 * http://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
 	 */
 	static final float ALPHA = 0.25f; // if ALPHA = 1 OR 0, no filter applies.
-   
-    
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see io.github.data4all.smoothing.Smoother#lowPass(float[], float[])
-     */
-    @Override
-    public float[] filter(float[] oldValues, float[] filteredValues) {
-        if (filteredValues == null)
-            return oldValues;
 
-        for (int i = 0; i < oldValues.length; i++) {
-            filteredValues[i] = filteredValues[i] + ALPHA * (oldValues[i] - filteredValues[i]);
-        }
-        return filteredValues;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.github.data4all.smoothing.Smoother#lowPass(float[], float[])
+	 */
+	@Override
+	public float[] filter(float[] input, float[] output) {
+		float[] filteredValues = new float[3];
+		if (output == null)
+			return input;
+
+		for (int i = 0; i < input.length; i++) {
+			filteredValues[i] = output[i] + ALPHA
+					* (input[i] - output[i]);
+		}
+		return filteredValues;
+	}
 }
