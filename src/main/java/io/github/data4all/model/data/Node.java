f736501c2567d3cdf5fb982c0f88e93c0b377d58
Felix Kirchgeorg <fkirchge@informatik.uni-bremen.de>
2015-01-23 20:24:21 +0100

    Node implements DataElement refs #3053

--- src/main/java/io/github/data4all/model/data/Node.java
+++ src/main/java/io/github/data4all/model/data/Node.java
@@ -11,7 +11,7 @@ import android.os.Parcelable;
  * @author fkirchge
  *
  */
-public class Node extends OsmElement {
+public class Node extends DataElement {
 
     /**
      * Latitude of the Node.
