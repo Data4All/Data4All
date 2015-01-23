f05ac333b001e4fa13213ccf53543e618f9b214d
Steeve <nkemson@tzi.de>
2015-01-23 12:50:03 +0100

    #2544 modified some tests and removed unecessary tests methods

--- src/test/java/io/github/data4all/activity/SensorReactionActivityTest.java
+++ src/test/java/io/github/data4all/activity/SensorReactionActivityTest.java
@@ -1,10 +1,6 @@
 package io.github.data4all.activity;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import io.github.data4all.logger.Log;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +10,6 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import android.content.Intent;
-import android.view.MotionEvent;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
@@ -22,9 +17,7 @@ public class SensorReactionActivityTest {
 
 	private SensorReactionActivity sensorReactionActivity;
 	private boolean mActivityStarted = false;
-	private boolean mActivityStopped = false;
-	private boolean isTouched;
-	private MotionEvent event;
+	
 
 	@Before
 	public void setUp() throws Exception {
@@ -50,45 +43,8 @@ public class SensorReactionActivityTest {
 					SensorReactionActivity.class);
 			sensorReactionActivity.startActivity(startIntent);
 			mActivityStarted = true;
-			assertTrue(mActivityStarted);
+			
 		}
 
-	}
-
-	/**
-	 * check if Activity was stopped
-	 * @throws Exception
-	 */
-	@Test
-	public void checkActivityStopped() throws Exception {
-		checkActivityStarted();
-		if (mActivityStarted || mActivityStopped) {
-			sensorReactionActivity.onStop();
-			mActivityStopped = true;
-		}
-
-	}
-    
-	/**
-	 * check if Screen has been touching
-	 * @throws Exception
-	 */
-	@Test
-	public void checkScreenTouched() throws Exception {
-		checkActivityStarted();
-		if (sensorReactionActivity.onTouchEvent(event)) {
-			isTouched = true;
-		}
-	}
-
-	/**
-	 * get Time, when screen has been touching
-	 * @throws Exception
-	 */
-	@Test
-	public void getTimeWhenScreenBeenTouched() throws Exception {
-		checkScreenTouched();
-		long startTime = System.currentTimeMillis();
-		assertEquals(System.currentTimeMillis(), startTime);
 	}
 }
--- src/test/java/io/github/data4all/service/OrientationListenerTest.java
+++ src/test/java/io/github/data4all/service/OrientationListenerTest.java
@@ -1,23 +1,20 @@
+
 package io.github.data4all.service;
-
-
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Random;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
 import android.content.Intent;
+
+/**
+ * 
+ * @author Steeve
+ *
+ */
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
@@ -28,18 +25,16 @@ public class OrientationListenerTest {
 	// examine if the Service was started
 	private boolean myServiceStarted = false;
 
-	// examine if the service was created
+	// check if the service was created
 	private boolean myServiceCreated = false;
 	
-	private boolean myServiceStopped = true;
+	//check if the service was stopped
+	private boolean myServiceStopped = false;
 
 	private boolean myServiceBound = false;
 
-	private boolean myServiceAttached = false;
-
 	private Intent myServiceIntent = null;
-    
-	private int myServiceId;
+
 	/*
 	 * return the actual service under Test
 	 */
@@ -52,27 +47,14 @@ public class OrientationListenerTest {
 	@Before
 	public void setUp() throws Exception {
 
-		Intent startIntent = new Intent(Robolectric.application,
-				OrientationListener.class);
-		OrientationListener orientationListener = new OrientationListener();
-		orientationListener.onStartCommand(startIntent, 0, 0);
-	}
-
-	/*
-	 * create the service under test
-	 */
-	@Test
-	public void setUpService() {
 		myService = null;
 		try {
 			myService = new OrientationListener();
 		} catch (Exception exception) {
 			assertNotNull(myService);
 		}
-		 myServiceId = new Random().nextInt();
-	        myServiceAttached = true;
 	}
-
+	
 	/**
 	 * start the service under test
 	 * this will be automatically stopped by the method tearDown
@@ -81,18 +63,23 @@ public class OrientationListenerTest {
 	public void startServiceTest() {
 		Intent startIntent = new Intent(Robolectric.application,
 				OrientationListener.class);
-		assertFalse(myServiceStarted);
-		assertFalse(myServiceBound);
-
-		if (!myServiceAttached) {
-			setUpService();
-		}
-		assertNotNull(myService);
 		myService.onStartCommand(startIntent, 0, 0);
 		myServiceCreated = true;
-		assertTrue(myServiceCreated);
+		myServiceStarted= true;
+		myServiceBound = true;
 	}
     
+	
+	/**
+	 * check if the Service was launched
+	 * @throws Exception
+	 */
+	@Test
+	public void testIfServiceWasLaunched() throws Exception {
+		assertNotNull("service should be launched successfully", myService);
+	}
+	
+	
 	/**
 	 *  this method will be called to stop the service under test
 	 *  this will stop the service,when this service was created or when he was started
@@ -101,6 +88,7 @@ public class OrientationListenerTest {
 	public void ShutDownServiceTest() {
 		if (myServiceCreated) {
 			myService.onDestroy();
+			myServiceCreated = false;
 		}
 		else if (myServiceStarted) {
 			myService.stopSelf();
@@ -108,29 +96,17 @@ public class OrientationListenerTest {
 		} else if (myServiceBound) {
 			myService.onUnbind(myServiceIntent);
 			myServiceBound = false;
-		}
-		
+		}	
 	}
 	
 	/**
 	 * Shut down the Service under Test and make sure that all resources are cleaned up
 	 * @throws Exception
 	 */
-	@Test
+	@After
 	public void tearDown() throws Exception{
-		ShutDownServiceTest();
 		myService = null;
-		assertTrue(myServiceStopped);
+		myServiceStopped = true;
 	}
     
-	/**
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testIfServiceWasLaunched() throws Exception {
-		setUpService();
-		assertNotNull("service should be launched successfully", myService);
-	}
-
 }
