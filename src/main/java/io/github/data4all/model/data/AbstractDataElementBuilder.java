d249df5baf9bf86ef5d5c2366dbd14bf543a1fdb
Felix Kirchgeorg <fkirchge@informatik.uni-bremen.de>
2015-01-24 19:19:57 +0100

    rename DataElement to AbstractDataElement refs #3053

--- src/main/java/io/github/data4all/model/data/AbstractDataElementBuilder.java
+++ src/main/java/io/github/data4all/model/data/AbstractDataElementBuilder.java
@@ -5,7 +5,7 @@ import android.util.SparseArray;
 
 /**
  * This class provides methods to write and read subclasses of
- * {@link DataElement} to and from a {@link Parcel}<br/>
+ * {@link AbstractDataElement} to and from a {@link Parcel}<br/>
  * <br/>
  * Currently supported subclasses:
  * <ul>
@@ -18,36 +18,36 @@ import android.util.SparseArray;
  *
  * @see android.os.Parcelable Parcelable
  */
-public final class DataElementBuilder {
+public final class AbstractDataElementBuilder {
     /**
-     * Reads the specific {@link DataElement} from the {@link Parcel}.
+     * Reads the specific {@link AbstractDataElement} from the {@link Parcel}.
      * 
      * @param parcel
      *            The {@link Parcel} to read the object's data from
-     * @return The read {@link DataElement} or {@code null}, if {@code null} was
+     * @return The read {@link AbstractDataElement} or {@code null}, if {@code null} was
      *         stored
      * 
      * @see android.os.Parcelable Parcelable
      */
-    public static final DataElement read(Parcel parcel) {
+    public static final AbstractDataElement read(Parcel parcel) {
         int osmId = parcel.readInt();
 
         if (osmId == NULL_OBJECT) {
             return null;
         } else {
-            Class<? extends DataElement> elementClass = IDS.get(osmId);
+            Class<? extends AbstractDataElement> elementClass = IDS.get(osmId);
 
             if (elementClass == null) {
                 throw new IllegalStateException("OsmObject is not in ID-Map");
             } else {
-                return (DataElement) parcel.readParcelable(elementClass
+                return (AbstractDataElement) parcel.readParcelable(elementClass
                         .getClassLoader());
             }
         }
     }
 
     /**
-     * Writes the specific {@link DataElement} to the {@link Parcel}
+     * Writes the specific {@link AbstractDataElement} to the {@link Parcel}
      * 
      * @param parcel
      *            The {@link Parcel} in which the object should be written
@@ -58,21 +58,21 @@ public final class DataElementBuilder {
      * 
      * @see android.os.Parcelable Parcelable
      */
-    public static final void write(Parcel parcel, DataElement element, int flags) {
+    public static final void write(Parcel parcel, AbstractDataElement element, int flags) {
         if (element == null) {
             parcel.writeInt(NULL_OBJECT);
         } else {
             int id = IDS.keyAt(IDS.indexOfValue(element.getClass()));
 
             parcel.writeInt(id);
-            parcel.writeParcelfable(element, flags);
+            parcel.writeParcelable(element, flags);
         }
     }
 
     /**
-     * Maps integer ids to subclasses of DataElement
+     * Maps integer ids to subclasses of AbstractDataElement
      */
-    private static final SparseArray<Class<? extends DataElement>> IDS = new SparseArray<Class<? extends DataElement>>();
+    private static final SparseArray<Class<? extends AbstractDataElement>> IDS = new SparseArray<Class<? extends AbstractDataElement>>();
 
     /**
      * The id which indicates that a {@code null}-reference is stored
@@ -84,13 +84,13 @@ public final class DataElementBuilder {
      */
     static {
         IDS.put(1, Node.class);
-        IDS.put(2, Way.class);
-        IDS.put(3, Relation.class);
+        IDS.put(2, PolyElement.class);
+        //IDS.put(3, Relation.class);
     }
 
     /**
      * Private constructor, prevents this class from being instantiated
      */
-    private DataElementBuilder() {
+    private AbstractDataElementBuilder() {
     }
 }
