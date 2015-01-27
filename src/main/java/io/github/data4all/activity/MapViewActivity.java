0d94663c03b2569bfb5ad49fe373ca525d20e5fb
konerman <konerman@tzi.de>
2015-01-27 14:34:55 +0100

    MapviewActivity binds with Gpsservice #refs 3065

--- src/main/java/io/github/data4all/activity/MapViewActivity.java
+++ src/main/java/io/github/data4all/activity/MapViewActivity.java
@@ -4,18 +4,21 @@ import io.github.data4all.R;
 import io.github.data4all.logger.Log;
 import io.github.data4all.model.data.Node;
 import io.github.data4all.service.GPSservice;
+import io.github.data4all.service.GPSservice.LocalBinder;
 
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 /**
  * Main Activity that shows the default mapview
@@ -25,173 +28,216 @@ import android.widget.Toast;
  */
 public class MapViewActivity extends MapActivity implements OnClickListener {
 
-	//Logger Tag
-	private static final String TAG = "MapViewActivity";
-	
-	/**
-	 * Called when the activity is first created.
-	 * 
-	 * @param savedInstanceState
-	 *            If the activity is being re-initialized after previously being
-	 *            shut down then this Bundle contains the data it most recently
-	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
-	 *            is null.</b>
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map_view);
-		setUpMapView();
-		
-		// Set Overlay for the actual Position
-		Log.i(TAG, "Added User Location Overlay to the map");
-		mapView.getOverlays().add(myLocationOverlay);
-		// Set ImageView for Loading Screen
-		view = (ImageView) findViewById(R.id.imageView1);
-		
-		//for setting the actualZoomLevel and Center Position on Orientation Change
-		if (savedInstanceState != null) {
-			//Zoom Level
-			if (savedInstanceState.getSerializable("actualZoomLevel") != null) {
-				actualZoomLevel = (Integer) savedInstanceState
-						.getSerializable("actualZoomLevel");
-			}
-			//Center Position
-			if (savedInstanceState.getSerializable("actualCenterLongitude") != null
-					&& savedInstanceState
-							.getSerializable("actualCenterLatitude") != null) {
-				actualCenterLatitude = (Double) savedInstanceState
-						.getSerializable("actualCenterLatitude");
-				actualCenterLongitude = (Double) savedInstanceState
-						.getSerializable("actualCenterLongitude");
-				actualCenter = new GeoPoint(actualCenterLatitude,
-						actualCenterLongitude);
-			}
-			view.setVisibility(View.GONE);
-		} else {
-			//fading out the loading screen
-			view.animate().alpha(0.0F).setDuration(1000).setStartDelay(1500)
-					.withEndAction(new Runnable() {
-						public void run() {
-							view.setVisibility(View.GONE);
-						}
-					}).start();
-		}
-		
-		//Set Zoomlevel and Center Position
-		Log.i(TAG, "Set Mapcenter to "
-				+ actualZoomLevel);
-		mapController.setZoom(actualZoomLevel);
+    // Logger Tag
+    private static final String TAG = "MapViewActivity";
 
-		// Set Listener for Buttons
+    /**
+     * Called when the activity is first created.
+     * 
+     * @param savedInstanceState
+     *            If the activity is being re-initialized after previously being
+     *            shut down then this Bundle contains the data it most recently
+     *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+     *            is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Start the GPS service
+        Log.i(TAG, "Start GPSService");
+        startService(new Intent(this, GPSservice.class));
+        // Bind to gpsservice
+        Intent intent = new Intent(this, GPSservice.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_map_view);
+        setUpMapView();
+
+        // Set Overlay for the actual Position
+        Log.i(TAG, "Added User Location Overlay to the map");
+        mapView.getOverlays().add(myLocationOverlay);
+        // Set ImageView for Loading Screen
+        view = (ImageView) findViewById(R.id.imageView1);
+
+        // for setting the actualZoomLevel and Center Position on Orientation
+        // Change
+        if (savedInstanceState != null) {
+            // Zoom Level
+            if (savedInstanceState.getSerializable("actualZoomLevel") != null) {
+                actualZoomLevel = (Integer) savedInstanceState
+                        .getSerializable("actualZoomLevel");
+            }
+            // Center Position
+            if (savedInstanceState.getSerializable("actualCenterLongitude") != null
+                    && savedInstanceState
+                            .getSerializable("actualCenterLatitude") != null) {
+                actualCenterLatitude = (Double) savedInstanceState
+                        .getSerializable("actualCenterLatitude");
+                actualCenterLongitude = (Double) savedInstanceState
+                        .getSerializable("actualCenterLongitude");
+                Log.d(TAG, "actualCenter in mapviewact,oncreate:"
+                        + actualCenterLatitude + "," + actualCenterLongitude);
+                actualCenter = new GeoPoint(actualCenterLatitude,
+                        actualCenterLongitude);
+            }
+            view.setVisibility(View.GONE);
+        } else {
+            // fading out the loading screen
+            view.animate().alpha(0.0F).setDuration(1000).setStartDelay(1500)
+                    .withEndAction(new Runnable() {
+                        public void run() {
+                            view.setVisibility(View.GONE);
+                        }
+                    }).start();
+        }
+
+        // Set Zoomlevel and Center Position
+        Log.i(TAG, "Set Mapcenter to " + actualZoomLevel);
+        mapController.setZoom(actualZoomLevel);
+
+        // Set Listener for Buttons
         ImageButton returnToPosition = (ImageButton) findViewById(R.id.return_to_actual_Position);
-		returnToPosition.setOnClickListener(this);
+        returnToPosition.setOnClickListener(this);
 
-		ImageButton satelliteMap = (ImageButton) findViewById(R.id.switch_maps);
-		satelliteMap.setOnClickListener(this);
+        ImageButton satelliteMap = (ImageButton) findViewById(R.id.switch_maps);
+        satelliteMap.setOnClickListener(this);
 
-		ImageButton camera = (ImageButton) findViewById(R.id.to_camera);
-		camera.setOnClickListener(this);
+        ImageButton camera = (ImageButton) findViewById(R.id.to_camera);
+        camera.setOnClickListener(this);
 
-		ImageButton newPoint = (ImageButton) findViewById(R.id.new_point);
-		newPoint.setOnClickListener(this);
+        ImageButton newPoint = (ImageButton) findViewById(R.id.new_point);
+        newPoint.setOnClickListener(this);
 
-	}
+    }
 
-	public void onClick(View v) {
-		switch (v.getId()) {
-		//Set center to user Location
-		case R.id.return_to_actual_Position:
-			if (myLocationOverlay.getMyLocation() != null) {
-				Log.i(TAG, "Set Mapcenter to"
-						+ myLocationOverlay.getMyLocation().toString());
-				mapController.setCenter(myLocationOverlay.getMyLocation());
-				mapView.postInvalidate();
-			}
-			break;
-		//Upload new Data	
-		case R.id.upload_data:
-			startActivity(new Intent(this, LoginActivity.class));
-			break;
-		//switch between Maps
-		case R.id.switch_maps:
-			// switch to OSM Map
-			if (mapView.getTileProvider().getTileSource().name()
-					.equals("MapBoxSatelliteLabelled")) {
-				Log.i(TAG, "Set Maptilesource to "
-						+ mapView.getTileProvider().getTileSource().name());
-				mapView.setTileSource(OSM_TILESOURCE);
-				ImageButton button = (ImageButton) findViewById(R.id.switch_maps);
-				button.setImageResource(R.drawable.ic_sat);
-				mapView.postInvalidate();
-				// switch to Satellite Map
-			} else {
-				Log.i(TAG, "Set Maptilesource to "
-						+ mapView.getTileProvider().getTileSource().name());
-				mapView.setTileSource(MAPBOX_SATELLITE_LABELLED);
-				ImageButton button = (ImageButton) findViewById(R.id.switch_maps);
-				button.setImageResource(R.drawable.ic_map);
-				mapView.postInvalidate();
-			}
-			break;
-		// Make Photo	
-		case R.id.to_camera:
-			startActivity(new Intent(this, CameraActivity.class));
-			break;
-		// Add new POI to the Map
-		case R.id.new_point:
-			GeoPoint myPosition = myLocationOverlay.getMyLocation();
-			Intent intent = new Intent(this, MapPreviewActivity.class);
-			Node poi = new Node(-1, 1, myPosition.getLatitude(),
-					myPosition.getLongitude());
-			
-			//Set Type Definition for Intent to Node
-			Log.i(TAG, "Set intent extra " + TYPE + " to " + NODE_TYPE_DEF);
-			intent.putExtra(TYPE, NODE_TYPE_DEF);
-			
-			//Set OsmElement for Intent to POI 
-			Log.i(TAG, "Set Intent Extra " + OSM + " to Node with Coordinates " + poi.toString());
-			intent.putExtra(OSM, poi);
-			
-			//Start MapPreview Activity
-			Log.i(TAG, "Start MapPreview Activity");
-			startActivity(intent);
-			break;
-		}
-	}
+    public void onClick(View v) {
+        switch (v.getId()) {
+        // Set center to user Location
+        case R.id.return_to_actual_Position:
+            if (myLocationOverlay.getMyLocation() != null) {
+                Log.i(TAG, "Set Mapcenter to"
+                        + myLocationOverlay.getMyLocation().toString());
+                mapController.setCenter(myLocationOverlay.getMyLocation());
+                mapView.postInvalidate();
+            }
+            break;
+        // Upload new Data
+        case R.id.upload_data:
+            startActivity(new Intent(this, LoginActivity.class));
+            break;
+        // switch between Maps
+        case R.id.switch_maps:
+            // switch to OSM Map
+            if (mapView.getTileProvider().getTileSource().name()
+                    .equals("MapBoxSatelliteLabelled")) {
+                Log.i(TAG, "Set Maptilesource to "
+                        + mapView.getTileProvider().getTileSource().name());
+                mapView.setTileSource(OSM_TILESOURCE);
+                ImageButton button = (ImageButton) findViewById(R.id.switch_maps);
+                button.setImageResource(R.drawable.ic_sat);
+                mapView.postInvalidate();
+                // switch to Satellite Map
+            } else {
+                Log.i(TAG, "Set Maptilesource to "
+                        + mapView.getTileProvider().getTileSource().name());
+                mapView.setTileSource(MAPBOX_SATELLITE_LABELLED);
+                ImageButton button = (ImageButton) findViewById(R.id.switch_maps);
+                button.setImageResource(R.drawable.ic_map);
+                mapView.postInvalidate();
+            }
+            break;
+        // Make Photo
+        case R.id.to_camera:
+            startActivity(new Intent(this, CameraActivity.class));
+            break;
+        // Add new POI to the Map
+        case R.id.new_point:
+            GeoPoint myPosition = myLocationOverlay.getMyLocation();
+            Intent intent = new Intent(this, MapPreviewActivity.class);
+            Node poi = new Node(-1, 1, myPosition.getLatitude(),
+                    myPosition.getLongitude());
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		
-		//Enable User Position display
-		Log.i(TAG, "Enable User Position Display");
-		myLocationOverlay.enableMyLocation();
-		
-		if(actualCenter!=null){
-			Log.i(TAG, "Set Mapcenter to " + actualCenter.toString());
-			mapController.setCenter(actualCenter);
-		}else if(myLocationOverlay.getMyLocation() != null){
-			actualCenter = myLocationOverlay.getMyLocation();
-			Log.i(TAG, "Set Mapcenter to " + actualCenter.toString());
-			mapController.setCenter(actualCenter);			
-		}
-		// Start the GPS tracking
-		Log.i(TAG, "Start GPSService");
-		startService(new Intent(this, GPSservice.class));
-	}
+            // Set Type Definition for Intent to Node
+            Log.i(TAG, "Set intent extra " + TYPE + " to " + NODE_TYPE_DEF);
+            intent.putExtra(TYPE, NODE_TYPE_DEF);
 
+            // Set OsmElement for Intent to POI
+            Log.i(TAG, "Set Intent Extra " + OSM + " to Node with Coordinates "
+                    + poi.toString());
+            intent.putExtra(OSM, poi);
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		
-		//Disable Actual Location Overlay
-		Log.i(TAG, "Disable Actual Location Overlay");
-		myLocationOverlay.disableMyLocation();
+            // Start MapPreview Activity
+            Log.i(TAG, "Start MapPreview Activity");
+            startActivity(intent);
+            break;
+        }
+    }
 
-		// Pause the GPS tracking
-		Log.i(TAG, "Stop GPSService");
-		stopService(new Intent(this, GPSservice.class));
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Enable User Position display
+        Log.i(TAG, "Enable User Position Display");
+        myLocationOverlay.enableMyLocation();
+
+        if (actualCenter != null) {
+            Log.i(TAG, "Set Mapcenter to " + actualCenter.toString());
+            mapController.setCenter(actualCenter);
+        } else if (myLocationOverlay.getMyLocation() != null) {
+            actualCenter = myLocationOverlay.getMyLocation();
+            Log.i(TAG, "Set Mapcenter to " + actualCenter.toString());
+            mapController.setCenter(actualCenter);
+        }
+        // Start the GPS tracking
+        // Log.i(TAG, "Start GPSService");
+        // startService(new Intent(this, GPSservice.class));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Disable Actual Location Overlay
+        Log.i(TAG, "Disable Actual Location Overlay");
+        myLocationOverlay.disableMyLocation();
+
+        // Pause the GPS tracking
+        Log.i(TAG, "Stop GPSService");
+        stopService(new Intent(this, GPSservice.class));
+    }
+
+    /**
+     * the connection with GPSservice. neccessary to make sure gpsservice is
+     * ready before we try to get a GPS position from it
+     * 
+     * @author: konerman
+     */
+    public ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+
+            Log.d(TAG, "binding with GPSservice complete ");
+            // We've bound to GPSservice, cast the IBinder and get
+            // GPSservice instance
+            LocalBinder binder = (LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+            // Set actual Center
+            if (getMyLocation() != null) {
+                Log.i(TAG, "Set actual Center to " + getMyLocation());
+                actualCenter = getMyLocation();
+                mapController.setCenter(actualCenter);
+
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+
+    };
 }
--- src/main/java/io/github/data4all/service/GPSservice.java
+++ src/main/java/io/github/data4all/service/GPSservice.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerManager;
@@ -30,18 +31,24 @@ public class GPSservice extends Service implements LocationListener {
     private LocationManager lmgr;
 
     private WakeLock wakeLock;
+    private Location lastKnownLocation;
+    // Binder given to clients
+    private final IBinder mBinder = new LocalBinder();
 
     @Override
     public void onCreate() {
+        Log.d("GPSSERVICE", "service started");
         // wakelock, so the cpu is never shut down and is able to track at all
         // time
         PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
         wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-                "MyWakelockTag");
+                "GPSservice");
         wakeLock.acquire();
 
         lmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        Optimizer.putLoc(findLastKnownLocation());
+        
         if (lmgr.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
             lmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             // second value is minimum of time, third value is minimum of meters
@@ -55,10 +62,26 @@ public class GPSservice extends Service implements LocationListener {
         }
     }
 
+    public Location findLastKnownLocation() {
+        Location loc = lmgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (loc != null) {
+            Log.d(this.getClass().getSimpleName(), "lastknownloc gps");
+            return loc;
+        }
+        loc = lmgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (loc != null) {
+            Log.d(this.getClass().getSimpleName(), "lastknownloc network");
+            return loc;
+        }
+        Log.d(this.getClass().getSimpleName(), "lastknownloc is null");
+        return null;
+
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
-
         return START_STICKY;
     }
 
@@ -91,10 +114,20 @@ public class GPSservice extends Service implements LocationListener {
                 .show();
     }
 
-    @Override
-    public IBinder onBind(Intent arg0) {
-        // TODO Auto-generated method stub
-        return null;
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
     }
 
+    
+    public class LocalBinder extends Binder {
+        public GPSservice getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return GPSservice.this;
+        }
+    }   
+    
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
 }
