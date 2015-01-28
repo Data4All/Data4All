1afde03743595e80b60fe66417ad1826570b6984
Timo Brose <tbrose@fb3.uni-bremen.de>
2015-01-28 22:16:33 +0100

    Added edgeDetection to the EdgeAnalyzer refs #2565

--- src/main/java/io/github/data4all/model/drawing/EdgeAnalyser.java
+++ src/main/java/io/github/data4all/model/drawing/EdgeAnalyser.java
@@ -36,7 +36,39 @@ public class EdgeAnalyser implements Analyzer<List<Point>> {
      */
     @Override
     public List<Point> analyze() {
-
+        
         return null;
+    }
+    
+    /**
+     * Detects all edges in the given bitmap.<br/>
+     * The brighter a pixel is the more different the pixel is to its neighbors.
+     * 
+     * @param input The image bitmap to analyze the edges
+     * @return A image providing all edges of the input bitmap
+     */
+    private static Bitmap edgeDetection(Bitmap input) {
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        Bitmap result = Bitmap.createBitmap(input.getWidth(), input.getHeight(), conf);
+        for(int x = input.getWidth() - 1; x >= 0; x--) {
+            for(int y = input.getHeight() - 1; y >= 0; y--) {
+                if(x == 0 || y == 0) {
+                    result.setPixel(x, y, 0xff000000);
+                } else {
+                    int rgb1 = input.getPixel(x, y);
+                    int rgb2 = input.getPixel(x-1, y);
+                    int rgb3 = input.getPixel(x, y-1);
+                    rgb1 = (rgb1&0xff) + ((rgb1>>8)&0xff) + ((rgb1>>16)&0xff);
+                    rgb2 = (rgb2&0xff) + ((rgb2>>8)&0xff) + ((rgb2>>16)&0xff);
+                    rgb3 = (rgb3&0xff) + ((rgb3>>8)&0xff) + ((rgb3>>16)&0xff);
+
+                    int dif = (int) Math.min(Math.hypot(rgb1-rgb2, rgb1-rgb3), 256);
+                    int rgb4 = (dif << 16) | (dif << 8) | dif;
+
+                    result.setPixel(x,y, 0xff000000 | rgb4);
+                }
+            }
+        }
+        return result;
     }
 }
