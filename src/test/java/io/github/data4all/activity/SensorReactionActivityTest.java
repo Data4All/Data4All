3f9f0137e9390967eb1123892064c32cf1553931
Steeve <nkemson@tzi.de>
2015-01-30 03:10:52 +0100

    added another tests ref #2544

--- src/test/java/io/github/data4all/activity/SensorReactionActivityTest.java
+++ src/test/java/io/github/data4all/activity/SensorReactionActivityTest.java
@@ -1,106 +1,139 @@
 package io.github.data4all.activity;
 
-import junit.textui.TestRunner;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricBase;
-import org.robolectric.RobolectricShadowOfLevel16;
-import org.robolectric.RobolectricShadowOfLevel9;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.res.builder.RobolectricPackageManager;
-import org.robolectric.shadows.RoboLayoutInflater;
 import org.robolectric.shadows.ShadowSensorManager;
 
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.view.MotionEvent;
-import static org.junit.Assert.*;
-//import static org.robolectric.Robolectric.shadowOf;
+
+/**
+ * Test cases for the SensorReactionActivity class
+ * @author Steeve
+ */
+
+
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
 public class SensorReactionActivityTest {
-
+    
+	//the activity to test
 	private SensorReactionActivity sensorReactionActivity;
+
+	//a object for the class shadowSensorManager
 	private ShadowSensorManager shadow;
-	SensorManager sensorManager;
+	private SensorManager sManager;
+	private SensorEvent event;
 	
+
 	@Before
 	public void setUp() throws Exception {
 
 		this.sensorReactionActivity = Robolectric
 				.buildActivity(SensorReactionActivity.class).create().get();
-		sensorManager = (SensorManager) sensorReactionActivity.getSystemService(Context.SENSOR_SERVICE);
-		
-	}
 
+		shadow = new ShadowSensorManager();
+		sManager = (SensorManager) Robolectric.application
+				.getSystemService(Context.SENSOR_SERVICE);
+		shadow.addSensor(Sensor.TYPE_ACCELEROMETER,
+				sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+	}
+    
 	@Test
 	public void checkActivityNotNull() throws Exception {
 		assertNotNull(sensorReactionActivity);
 	}
 	
-	@Test
-	public void touchEventStart (){
-		//create touch event
-		MotionEvent event  = MotionEvent.obtain(System.nanoTime(),System.nanoTime(), MotionEvent.ACTION_DOWN, 1.5f, 2.3f,  0);
-		sensorReactionActivity.onTouch(sensorReactionActivity.getCurrentFocus(), event);
-		assertTrue(sensorReactionActivity.getIsTouched());
-	}
-	
-	@Test
-	public void touchSensorEventStart (){
-		shadow = Robolectric.shadowOf(sensorManager);
-		//create a sensorevent
-		SensorEvent sevent=shadow.createSensorEvent();
-		
-		sevent.values[0]=1.1f;
-		sevent.values[1]=1.1f;
-		sevent.values[2]=1.1f;
-		sevent.timestamp=System.nanoTime();
-		
-		//first fired the event;
-		sensorReactionActivity.onSensorChanged(sevent);
-		//register the last position
-		assertTrue(sensorReactionActivity.getLastX()==1.1f);
-		assertTrue(sensorReactionActivity.getLastY()==1.1f);
-		assertTrue(sensorReactionActivity.getLastZ()==1.1f);
-		assertNull(sensorReactionActivity.showDialog);
-		
-		//create touch event
-		MotionEvent event  = MotionEvent.obtain(System.nanoTime(),System.nanoTime(), MotionEvent.ACTION_DOWN, 1.5f, 2.3f,  0);
-		sensorReactionActivity.onTouch(sensorReactionActivity.getCurrentFocus(), event);
-		assertTrue(sensorReactionActivity.getIsTouched());
-		
-		//create new SensorEvent
-		sevent=shadow.createSensorEvent();
-		sevent.values[0]=4.1f;
-		sevent.values[1]=1.1f;
-		sevent.values[2]=1.1f;
-		sevent.timestamp=System.nanoTime();
-		//second fired the event;
-		sensorReactionActivity.onSensorChanged(sevent);
-		assertNotNull(sensorReactionActivity.showDialog);
-		
-		
-		
-	}
-	
-	@Test
-	public void checkPositionMoved (){
-		//SensorEvent event = S
+	@After
+	public void tearDown() {
+		sensorReactionActivity = null;
+		sManager = null;
+		shadow = null;
 	}
     
-	
-	
-	
-	//ich muss die sensor werte beiinflüssen und prüfen was passiert
-	//shadowDialog.get gucken ob er ein Dialog geworfen hat.
-	// roboelectric sensor recherche addsensor()
-	//createsensorevent()
-	
+	/*
+	 * create a touch event  MotionEvent
+	 * and check if isTouched is true
+	 */
+	@Test
+	public void touchEventStart() {
+		// create touch event MotionEvent
+		MotionEvent event = MotionEvent.obtain(System.nanoTime(),
+				System.nanoTime(), MotionEvent.ACTION_DOWN, 1.5f, 2.3f, 0);
+		sensorReactionActivity.onTouch(
+				sensorReactionActivity.getCurrentFocus(), event);
+		assertTrue(sensorReactionActivity.getIsTouched());
+	}
+    
+	/*
+	 * check if the activity has a listener after Registering listener
+	 */
+	@Test
+	public void shouldReturnHasListenerAfterRegisteringListener() {
+		SensorEventListener listener = registerListener();
+		assertTrue(shadow.hasListener(listener));
+	}
+    
+	/*
+	 * to register a listener 
+	 */
+	private SensorEventListener registerListener() {
+		SensorEventListener listener = new SensorReactionActivity();
+		Sensor accelerometer = sManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		shadow.registerListener(listener, accelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		return listener;
+	}
+    
+	/*
+	 * check if the activity has always a listener after the listener was unregister
+	 */
+	@Test
+	public void shouldReturnHasNoListenerAfterUnregisterListener() {
+		SensorEventListener listener = registerListener();
+		shadow.unregisterListener(listener,
+				sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+		assertFalse(shadow.hasListener(listener));
+	}
+    
+	/*
+	 * check if the activity has a listener by Default
+	 */
+	@Test
+	public void shouldReturnHasNoListenerByDefault() {
+		SensorEventListener listener = new SensorReactionActivity();
+		assertFalse(shadow.hasListener(listener));
+	}
+    
+	/*
+	 * check if a AlertDialog appear by default, when phone not move
+	 */
+	@Test
+	public void shouldReturnHasNoDialogByDefault() {
+		event = shadow.createSensorEvent();	
+		event.timestamp = System.nanoTime();
+		assertNull(sensorReactionActivity.getShowDialog());
+	}
+    
+	/*
+	 * get a sensorevent
+	 */
+	public SensorEvent getEvent() {
+		return event;
+	}
 }
--- src/test/java/io/github/data4all/service/OrientationListenerTest.java
+++ src/test/java/io/github/data4all/service/OrientationListenerTest.java
@@ -1,6 +1,8 @@
-
 package io.github.data4all.service;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,105 +10,93 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.media.MediaScannerConnection;
+
+
 
 /**
- * 
- * @author Steeve
- *
- */
+* test cases for the OrientationListener class 
+* @author Steeve
+*
+*/
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
 public class OrientationListenerTest {
 
-	private OrientationListener myService;
+	// the service to test
+	private OrientationListener service;
 
-	// examine if the Service was started
-	private boolean myServiceStarted = false;
-
-	// check if the service was created
-	private boolean myServiceCreated = false;
-	
-	//check if the service was stopped
-	private boolean myServiceStopped = false;
-
-	private boolean myServiceBound = false;
-
-	private Intent myServiceIntent = null;
-
-	/*
-	 * return the actual service under Test
-	 */
-
-	public OrientationListener getService() {
-		return myService;
-	}
-
-	
+   
+    
+    public static final String SERVICE = "io.github.data4all.service.OrientationListener";
+    
+    
 	@Before
 	public void setUp() throws Exception {
+        service = null;
+        try{
+        	service = new OrientationListener();
+       
+        }catch(Exception  e){
+        	assertNotNull(service);
+        }	
+	}
+	
 
-		myService = null;
-		try {
-			myService = new OrientationListener();
-		} catch (Exception exception) {
-			assertNotNull(myService);
-		}
-	}
-	
 	/**
-	 * start the service under test
-	 * this will be automatically stopped by the method tearDown
-	 */
-	@Test
-	public void startServiceTest() {
-		Intent startIntent = new Intent(Robolectric.application,
-				OrientationListener.class);
-		myService.onStartCommand(startIntent, 0, 0);
-		myServiceCreated = true;
-		myServiceStarted= true;
-		myServiceBound = true;
-	}
-    
-	
-	/**
-	 * check if the Service was launched
-	 * @throws Exception
-	 */
-	@Test
-	public void testIfServiceWasLaunched() throws Exception {
-		assertNotNull("service should be launched successfully", myService);
-	}
-	
-	
-	/**
-	 *  this method will be called to stop the service under test
-	 *  this will stop the service,when this service was created or when he was started
-	 */
-	@Test
-	public void ShutDownServiceTest() {
-		if (myServiceCreated) {
-			myService.onDestroy();
-			myServiceCreated = false;
-		}
-		else if (myServiceStarted) {
-			myService.stopSelf();
-			myServiceStarted = false;
-		} else if (myServiceBound) {
-			myService.onUnbind(myServiceIntent);
-			myServiceBound = false;
-		}	
-	}
-	
-	/**
-	 * Shut down the Service under Test and make sure that all resources are cleaned up
+	 * Shut down the Service under Test and make sure that all resources are
+	 * cleaned up
+	 * 
 	 * @throws Exception
 	 */
 	@After
-	public void tearDown() throws Exception{
-		myService = null;
-		myServiceStopped = true;
+	public void tearDown() throws Exception {
+		service = null;
 	}
-    
+	
+	/**
+	 * start the service under test this will be automatically stopped by the
+	 * method tearDown
+	 */
+	@Test
+	public void should_startService() {
+		Intent startIntent = new Intent(Robolectric.application,
+				OrientationListener.class);
+		service.onStartCommand(startIntent, 0, 0);
+		assertEquals(SERVICE, startIntent
+                .getComponent().getClassName());
+	}
+	
+	
+	@Test
+	public void should_bindService_Successfully() {
+		Intent intent = new Intent(Robolectric.application,
+				OrientationListener.class);
+		ServiceConnection serviceConnection = Robolectric
+				.newInstanceOf(MediaScannerConnection.class);
+		service.bindService(intent,serviceConnection,0);
+	}
+	
+	@Test
+	public void should_stopService() {
+		
+		Intent intent = new Intent(Robolectric.application,
+				OrientationListener.class);
+		service.stopSelf();
+		service.stopService(intent);
+	}
+	
+	
+	@Test
+	public void should_UnbindService_Successfully() {
+
+		ServiceConnection serviceConnection = Robolectric
+				.newInstanceOf(MediaScannerConnection.class);
+		service.unbindService(serviceConnection);
+	}
+	
 }
