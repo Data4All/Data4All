9b9d2e484f64f437511955fdd737858ddd9174c5
Oliver Schwartz <schwarto@fb3.uni-bremen.de>
2015-01-30 14:06:31 +0100

    Fixes SonarQube Issues

--- src/main/java/io/github/data4all/activity/AbstractMapActivity.java
+++ src/main/java/io/github/data4all/activity/AbstractMapActivity.java
@@ -1,18 +1,18 @@
-/*******************************************************************************
+/** 
  * Copyright (c) 2014, 2015 Data4All
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ **/
 package io.github.data4all.activity;
 
 import io.github.data4all.R;
@@ -20,9 +20,9 @@ import io.github.data4all.logger.Log;
 import io.github.data4all.model.data.Node;
 import io.github.data4all.model.data.OsmElement;
 import io.github.data4all.model.data.Way;
-import io.github.data4all.model.map.MapLine;
-import io.github.data4all.model.map.MapMarker;
-import io.github.data4all.model.map.MapPolygon;
+import io.github.data4all.model.map.MapLine; //NOSONAR
+import io.github.data4all.model.map.MapMarker; //NOSONAR
+import io.github.data4all.model.map.MapPolygon; //NOSONAR
 
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IGeoPoint;
@@ -53,7 +53,7 @@ import android.widget.ImageView;
  * @author Oliver Schwartz
  *
  */
-public abstract class AbstractMapActivity extends BasicActivity {
+public abstract class AbstractMapActivity extends BasicActivity { //NOSONAR
 
     // Node Type Definition Number
     protected static final int NODE_TYPE_DEF = 1;
@@ -159,6 +159,12 @@ public abstract class AbstractMapActivity extends BasicActivity {
         myLocationOverlay = new MyLocationNewOverlay(this, mapView);
     }
 
+    /**
+     * Removes an Overlay from the Map.
+     *
+     * @param element
+     *            the Overlay which should be removed  from the map
+     **/
     public void removeOverlayFromMap(Overlay overlay) {
         if (mapView.getOverlays().contains(overlay)) {
             mapView.getOverlays().remove(overlay);
