de3a4fa9bc789a3b39f10441a8493eed667cdb38
Felix Kirchgeorg <fkirchge@informatik.uni-bremen.de>
2015-02-06 19:18:04 +0100

    removed duplicate method getBeta

--- src/main/java/io/github/data4all/model/drawing/Point.java
+++ src/main/java/io/github/data4all/model/drawing/Point.java
@@ -22,32 +22,6 @@ package io.github.data4all.model.drawing;
  * @author tbrose
  */
 public class Point {
-    /**
-     * Calculates the angle in Point b for the two lines (a,b) and (b,c)<br/>
-     * If any point is {@code null} a IllegalArgumentException is thrown
-     * 
-     * @param a
-     *            The first Point
-     * @param b
-     *            The second Point
-     * @param c
-     *            The third Point
-     * @return The angle in Point b in radians
-     * @throws
-     *         IllegalArgumentException If any parameter is {@code null}
-     */
-    public static double getBeta(Point a, Point b, Point c) {
-        // Calculate the two vectors
-        if (a != null && b != null && c != null) {
-            final Point x = new Point(a.getX() - b.getX(), a.getY() - b.getY());
-            final Point y = new Point(c.getX() - b.getX(), c.getY() - b.getY());
-            return Math.acos((x.getX() * y.getX() + x.getY() * y.getY())
-                    / (Math.hypot(x.getX(), x.getY()) * Math.hypot(y.getX(),
-                            y.getY())));
-        } else {
-            throw new IllegalArgumentException("parameters cannot be null");
-        }
-    }
 
     /**
      * The x-coordinate
