b4b92e3083c57e77e3436ce588c689090031fb1d
Andre Koch <andre.koch@hmmh.de>
2015-02-10 21:18:49 +0100

    refactor activity and change logging refs #3012

--- src/main/java/io/github/data4all/activity/CameraActivity.java
+++ src/main/java/io/github/data4all/activity/CameraActivity.java
@@ -26,159 +26,146 @@ import android.widget.Toast;
 
 public class CameraActivity extends Activity {
 
-    // Logger Tag
-    private static final String TAG = "CameraActivity";
+	// Logger Tag
+	private static final String TAG = CameraActivity.class.getSimpleName();
 
-    private Camera mCamera;
-    private CameraPreview cameraPreview;
-    private int deviceHeight;
-    private ImageButton btnCapture;
+	private Camera mCamera;
+	private CameraPreview cameraPreview;
+	private ImageButton btnCapture;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	Log.i(TAG, "onCreate is called");
-	super.onCreate(savedInstanceState);
-	requestWindowFeature(Window.FEATURE_NO_TITLE);
-	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-	setContentView(R.layout.activity_camera);
-
-	// Checking camera availability
-	Log.d(TAG, "check if device support Camera");
-	if (!isDeviceSupportCamera()) {
-	    finish();
-	    Toast.makeText(getApplicationContext(),
-		    getString(R.string.noCamSupported), Toast.LENGTH_LONG)
-		    .show();
-	    Log.d(TAG, "device supports no camera");
-
-	}
-	btnCapture = (ImageButton) findViewById(R.id.btnCapture);
-	btnCapture.setOnClickListener(btnCaptureOnClickListener);
-    }
-
-    private OnClickListener btnCaptureOnClickListener = new OnClickListener() {
 	@Override
-	public void onClick(View v) {
-	    mCamera.takePicture(shutterCallback, null,
-		    new CapturePictureHandler(getApplicationContext()));
-	}
-    };
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "onCreate is called");
+		super.onCreate(savedInstanceState);
 
-    @Override
-    protected void onResume() {
-	Log.i(TAG, "onResume is called");
-	super.onResume();
+		// remove title and status bar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-	if (mCamera == null) {
-	    Log.d(TAG, "camera is null, so we have to create a new one");
-	    createCamera();
-	}
+		setContentView(R.layout.activity_camera);
 
-    }
-
-    @Override
-    protected void onPause() {
-	Log.i(TAG, "onPause is called");
-	super.onPause();
-
-	if (mCamera != null) {
-	    Log.d(TAG, "camera is not null, set everything to null");
-	    cameraPreview.setCamera(null);
-	    releaseCamera();
+		// Checking camera availability
+		Log.d(TAG, "check if device support Camera");
+		if (!isDeviceSupportCamera()) {
+			Toast.makeText(getApplicationContext(),
+					getString(R.string.noCamSupported), Toast.LENGTH_LONG)
+					.show();
+			finish();
+			Log.d(TAG, "device supports no camera");
+			return;
+		}
+		
+		btnCapture = (ImageButton) findViewById(R.id.btnCapture);
+		btnCapture.setOnClickListener(btnCaptureOnClickListener);
 	}
 
-	btnCapture.setOnClickListener(null);
-	btnCapture.setClickable(false);
+	private OnClickListener btnCaptureOnClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			mCamera.takePicture(shutterCallback, null,
+					new CapturePictureHandler(getApplicationContext()));
+		}
+	};
 
-    }
+	@Override
+	protected void onResume() {
+		Log.i(TAG, "onResume is called");
+		
+		super.onResume();
 
-    private void createCamera() {
-	Log.i(TAG, "createCamera is called");
+		if (mCamera == null) {
+			Log.d(TAG, "camera is null, so we have to recreate");
+			createCamera();
+		}
 
-	if (isDeviceSupportCamera()) {
-	    Log.d(TAG, "seems as the device has a camera");
-
-	    mCamera = getCameraInstance();
-
-	    cameraPreview = (CameraPreview) findViewById(R.id.cameraPreview);
-	    
-	    Log.d(TAG, "now set camera");
-	    if (mCamera == null) {
-		Log.d(TAG, "shit,camera is null");
-		finish();
-		return;
-	    }
-	    Log.d(TAG, "DEBUG " + mCamera.hashCode());
-	    cameraPreview.setCamera(mCamera);
-
-	    Log.d(TAG, "finish create Camera");
-	} else {
-
-	    Log.e(TAG, "Cant believe");
-	}
-    }
-
-    /**
-     * This method looks whether the device has a camera and then returns a
-     * boolean.
-     * 
-     * @return boolean true if device has a camera, false otherwise
-     */
-    private boolean isDeviceSupportCamera() {
-	Log.d(TAG, "look if device has camera");
-	return getApplicationContext().getPackageManager().hasSystemFeature(
-		PackageManager.FEATURE_CAMERA);
-    }
-
-    private void releaseCamera() {
-	Log.d(TAG, "release Camera is called");
-	if (mCamera != null) {
-	    mCamera.release(); // release the camera for other applications
-	    mCamera = null;
-	}
-    }
-
-    @Override
-    protected void onDestroy() {
-	super.onDestroy();
-
-	releaseCameraAndPreview();
-    }
-
-    /**
-     * A safe way to get an instance of the Camera object.
-     */
-    public Camera getCameraInstance() {
-	Log.d(TAG, "get camera instance is called");
-	Camera camera = null;
-	try {
-	    releaseCameraAndPreview();
-	    camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
-
-	} catch (Exception e) {
-	    Log.e(TAG, "failed to open Camera");
-	    e.printStackTrace();
 	}
 
-	return camera;
-    }
+	@Override
+	protected void onPause() {
+		Log.i(TAG, "onPause is called");
+		super.onPause();
 
+		if (mCamera != null) {
+			Log.d(TAG, "camera is not null on pause, so release it");
+			cameraPreview.setCamera(null);
+			releaseCamera();
+		}
 
-    
-    private void releaseCameraAndPreview() {
-	//cameraPreview.setCamera(null);
-	if (mCamera != null) {
-	    mCamera.release();
-	    mCamera = null;
+		btnCapture.setOnClickListener(null);
+		btnCapture.setClickable(false);
+
 	}
-    }
+	
+	/**
+	 * This method setup the camera.It calls a method from @CameraPreview and sets all camera parameters.
+	 */
+	private void createCamera() {
+		Log.i(TAG, "createCamera is called");
 
-    ShutterCallback shutterCallback = new ShutterCallback() {
-	public void onShutter() {
-	    // Log.d(TAG, "onShutter'd");
+		if (isDeviceSupportCamera()) {
+			mCamera = getCameraInstance();
+			cameraPreview = (CameraPreview) findViewById(R.id.cameraPreview);
+			cameraPreview.setCamera(mCamera);
+		} else {
+			Log.e(TAG, "Device not support camera");
+		}
 	}
-    };
+
+	
+	/**
+	 * This method looks whether the device has a camera and then returns a
+	 * boolean.
+	 * 
+	 * @return boolean true if device has a camera, false otherwise
+	 */
+	private boolean isDeviceSupportCamera() {
+		Log.d(TAG, "look if device has camera");
+		return getApplicationContext().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA);
+	}
+
+	
+	/**
+	 * This method release the camera for other applications and set camera to null.
+	 */
+	private void releaseCamera() {
+		Log.d(TAG, "release Camera is called");
+		if (mCamera != null) {
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		releaseCamera();
+	}
+
+	/**
+	 * A safe way to get an instance of the Camera object.
+	 */
+	public Camera getCameraInstance() {
+		Log.d(TAG, "try to get an instance of camera");
+		Camera camera = null;
+		try {
+			releaseCamera();
+			camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+
+		} catch (Exception e) {
+			Log.e(TAG, "failed to get an instance of camera");
+			e.printStackTrace();
+		}
+
+		return camera;
+	}
+
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+
+		}
+	};
 
 }
--- src/main/java/io/github/data4all/view/CameraPreview.java
+++ src/main/java/io/github/data4all/view/CameraPreview.java
@@ -1,19 +1,18 @@
 package io.github.data4all.view;
 
 import io.github.data4all.logger.Log;
+import io.github.data4all.util.CameraUtil;
 
 import java.io.IOException;
 import java.util.List;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.ImageFormat;
-import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.hardware.Camera.Size;
 import android.util.AttributeSet;
 import android.view.Display;
-import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -31,332 +30,298 @@ import android.view.WindowManager;
 
 public class CameraPreview extends ViewGroup implements SurfaceHolder.Callback {
 
-    private static final String TAG = CameraPreview.class.getSimpleName();
+	private static final String TAG = CameraPreview.class.getSimpleName();
 
-    private SurfaceHolder mHolder;
-    private Camera mCamera;
-    private Size mPreviewSize;
-    private Size mPhotoSize;
-    private List<Size> mSupportedPreviewSizes;
-    private List<Size> mSupportedPictureSizes;
-    private List<String> mSupportedFlashModes;
-    private List<String> mAutoFocus;
-    public static int containerWidth = 0;
-    public static int containerHeight = 0;
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+	private Size mPreviewSize;
+	private Size mPhotoSize;
+	private Size orientationPreviewSize;
+	private List<Size> mSupportedPreviewSizes;
+	private List<Size> mSupportedPictureSizes;
+	private List<String> mSupportedFlashModes;
+	private List<String> mAutoFocus;
 
-    private int orientation;
+	public static int containerWidth = 0;
+	public static int containerHeight = 0;
 
-    private Context context;
-    public CameraPreview(Context context) {
-	super(context);
-	init(context);
-    }
+	private Context context;
 
-    public CameraPreview(Context context, AttributeSet attributeSet) {
-	super(context, attributeSet);
-	init(context);
-
-    }
-
-    public CameraPreview(Context context, AttributeSet attrs, int defStyle) {
-	super(context, attrs, defStyle);
-	init(context);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-	Log.i(TAG, "onMeasure is called");
-
-	super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-	final int width = resolveSize(getSuggestedMinimumWidth(),
-		widthMeasureSpec);
-	final int height = resolveSize(getSuggestedMinimumHeight(),
-		heightMeasureSpec);
-
-	Log.d(TAG, "### found width: " + containerWidth);
-	Log.d(TAG, "### found height: " + containerHeight);
-
-	setMeasuredDimension(width, height);
-
-	if (containerWidth < width || containerHeight < height) {
-	    containerWidth = width;
-	    containerHeight = height;
+	public CameraPreview(Context context) {
+		super(context);
+		init(context);
 	}
 
-	Log.d(TAG, "### found containerwidth: " + containerWidth);
-	Log.d(TAG, "### found containerheight: " + containerHeight);
+	public CameraPreview(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+		init(context);
 
-	if (mSupportedPreviewSizes != null) {
-	    mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes,
-		    containerWidth, containerHeight);
 	}
 
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
-	if (changed && getChildCount() > 0) {
-	    final View child = getChildAt(0);
-
-	    final int width = r - l;
-	    final int height = b - t;
-
-	    int previewWidth = width;
-	    int previewHeight = height;
-            if (mPreviewSize != null)
-            {
-                Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-                Log.d(TAG, "ORIENTATION :"+display.getRotation());
-                switch (display.getRotation())
-                {
-                    case Surface.ROTATION_0:
-                        previewWidth = mPreviewSize.height;
-                        previewHeight = mPreviewSize.width;
-                       
-                        break;
-                    case Surface.ROTATION_90:
-                        previewWidth = mPreviewSize.width;
-                        previewHeight = mPreviewSize.height;
-                        break;
-                    case Surface.ROTATION_180:
-                        previewWidth = mPreviewSize.height;
-                        previewHeight = mPreviewSize.width;
-                        break;
-                    case Surface.ROTATION_270:
-                        previewWidth = mPreviewSize.width;
-                        previewHeight = mPreviewSize.height;
-                        
-                        break;
-                }
-                orientation = display.getRotation();
-            }
-
-            final int scaledChildHeight = previewHeight * width / previewWidth;
-            child.layout(0, height - scaledChildHeight, width, height);
-	}
-    }
-
-    
-    private void init(Context context) {
-
-	SurfaceView mSurfaceView = new SurfaceView(context);
-	addView(mSurfaceView);
-	mHolder = mSurfaceView.getHolder();
-	mHolder.addCallback(this);
-	this.context = context;
-    }
-
-    public void setCamera(Camera camera) {
-	mCamera = camera;
-	if (mCamera != null) {
-	    // get a group of supported preview size
-	    mSupportedPreviewSizes = mCamera.getParameters()
-		    .getSupportedPreviewSizes();
-
-	    mSupportedPictureSizes = mCamera.getParameters()
-		    .getSupportedPictureSizes();
-
-	    mSupportedFlashModes = mCamera.getParameters()
-		    .getSupportedFlashModes();
-
-	    mAutoFocus = mCamera.getParameters().getSupportedFocusModes();
-
-	    requestLayout();
+	public CameraPreview(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context);
 	}
 
-    }
+	private void init(Context context) {
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-	Log.i(TAG, "surfaceCreated is called");
+		SurfaceView mSurfaceView = new SurfaceView(context);
+		addView(mSurfaceView);
+		mHolder = mSurfaceView.getHolder();
+		mHolder.addCallback(this);
+		this.context = context;
+	}
 
-	try {
-	    if (mCamera != null) {
-		mCamera.setPreviewDisplay(holder);
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		Log.d(TAG, "onMeasure is called");
 
-		// retrieve the parameters of cameras
-		Camera.Parameters params = mCamera.getParameters();
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-		// set the preview size for display photo
-		Log.d(TAG, "calcuated mPreviewSize width=" + mPreviewSize.width
-			+ ", height=" + mPreviewSize.height);
+		final int width = resolveSize(getSuggestedMinimumWidth(),
+				widthMeasureSpec);
+		final int height = resolveSize(getSuggestedMinimumHeight(),
+				heightMeasureSpec);
 
-		params.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+		// Log.d(TAG, "found width: " + width);
+		// Log.d(TAG, "found height: " + height);
 
-		mPhotoSize = getOptimalPictureSize(mSupportedPictureSizes,
-			(mPreviewSize.width * 1.0)
-				/ (mPreviewSize.height * 1.0));
+		setMeasuredDimension(width, height);
 
-		// set the picture size for taking photo
-		Log.d(TAG, "########## calcuated Photo width="
-			+ mPhotoSize.width + ", height=" + mPhotoSize.height);
-
-		params.setPictureSize(mPhotoSize.width, mPhotoSize.height);
-
-		// set the picture type for taking photo
-		params.setPictureFormat(ImageFormat.JPEG);
-		params.set("jpeg-quality", 90);
-
-		// if auto focus support, use auto focus
-		if (mAutoFocus
-			.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
-		    Log.i(TAG, "############################");
-		    params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-		} else if (mAutoFocus
-			.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
-		    Log.i(TAG, "#####+++++++######");
-		    params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+		if (containerWidth < width || containerHeight < height) {
+			containerWidth = width;
+			containerHeight = height;
 		}
 
-		// if auto flash support, use flash
+		// Log.d(TAG, "containerwidth: " + containerWidth);
+		// Log.d(TAG, "containerheight: " + containerHeight);
+
+		if (mSupportedPreviewSizes != null) {
+			mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes,
+					containerWidth, containerHeight);
+		}
+
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		Log.d(TAG, "onLayout is called");
+
+		if (changed && getChildCount() > 0) {
+			final View child = getChildAt(0);
+
+			final int width = r - l;
+			final int height = b - t;
+
+			int previewWidth = width;
+			int previewHeight = height;
+
+			orientationPreviewSize = mCamera.new Size(previewHeight,
+					previewHeight);
+
+			if (mPreviewSize != null) {
+				CameraUtil.getRotationAndSetPreviewSize(context, mPreviewSize,
+						orientationPreviewSize);
+			}
+
+			final int scaledChildHeight = previewHeight * width / previewWidth;
+			child.layout(0, height - scaledChildHeight, width, height);
+		}
+	}
+
+	public void setCamera(Camera camera) {
+		Log.d(TAG, "setCamera is called");
+
+		mCamera = camera;
+		if (mCamera != null) {
+			// get a group of supported preview size
+			mSupportedPreviewSizes = mCamera.getParameters()
+					.getSupportedPreviewSizes();
+
+			mSupportedPictureSizes = mCamera.getParameters()
+					.getSupportedPictureSizes();
+
+			mSupportedFlashModes = mCamera.getParameters()
+					.getSupportedFlashModes();
+
+			mAutoFocus = mCamera.getParameters().getSupportedFocusModes();
+
+			requestLayout();
+		}
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d(TAG, "surfaceCreated is called");
+		try {
+			if (mCamera != null) {
+				mCamera.setPreviewDisplay(holder);
+
+				// retrieve the parameters of cameras
+				Camera.Parameters params = mCamera.getParameters();
+
+				// set the preview size for display photo
+				Log.d(TAG, "calcuated mPreviewSize width=" + mPreviewSize.width
+						+ ", height=" + mPreviewSize.height);
+
+				params.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+
+				mPhotoSize = getOptimalPictureSize(mSupportedPictureSizes,
+						(mPreviewSize.width * 1.0)
+								/ (mPreviewSize.height * 1.0));
+
+				// set the picture size for the taking photo
+				Log.d(TAG, "########## calcuated Photo width="
+						+ mPhotoSize.width + ", height=" + mPhotoSize.height);
+
+				params.setPictureSize(mPhotoSize.width, mPhotoSize.height);
+
+				// set the picture type for taking photo
+				params.setPictureFormat(ImageFormat.JPEG);
+				params.set("jpeg-quality", 90);
+
+				setFocusModes(params);
+
+				setFlashModes(params);
+
+				// if auto flash support, use flash
+
+				mCamera.setParameters(params);
+			}
+		} catch (IOException ex) {
+			Log.e(TAG, "IOException caused by setPreviewDisplay()", ex);
+		}
+
+	}
+
+	private Camera.Parameters setFlashModes(Camera.Parameters params) {
+		Log.d(TAG, "setFlashModes is called");
+
 		if (mSupportedFlashModes != null
-			&& mSupportedFlashModes
-				.contains(Camera.Parameters.FLASH_MODE_AUTO))
-		    params.setFocusMode(Camera.Parameters.FLASH_MODE_AUTO);
-		mCamera.setParameters(params);
-	    }
-	} catch (IOException ex) {
-	    Log.e(TAG, "IOException caused by setPreviewDisplay()", ex);
-	}
-
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-	if (mCamera != null) {
-	    mCamera.autoFocus(null);
-	}
-	return true;
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-	    int height) {
-
-	Log.d(TAG, "surfaceChanged is called");
-
-	try {
-	    mCamera.stopPreview();
-	} catch (Exception e) {
-	    Log.e(TAG, " tried to stop a non-existent preview");
-	}
-
-	if (mCamera == null) {
-	    Log.e(TAG, " mCamera is null");
-	    return;
-	}
-
-	mCamera.setDisplayOrientation(getOrientation());
-	mCamera.startPreview();
-
-    }
-    
-    private int getOrientation() {
-	int result = 0;
-        switch (orientation)
-        {
-            case Surface.ROTATION_0:
-                result = 90;
-                break;
-            case Surface.ROTATION_90:
-                result = 0;
-                break;
-            case Surface.ROTATION_180:
-                result = 90;
-                break;
-            case Surface.ROTATION_270:
-                result = 180;
-                break;
-        }
-        return result;
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-	if (mCamera != null) {
-	    mCamera.stopPreview();
-	}
-
-    }
-
-    /*
-     * @Function: get optimal picture size according to camera view angles
-     */
-    private Size getOptimalPictureSize(List<Size> sizes, double targetRatio) {
-	Log.d(TAG, "###### picture targetRatio=" + targetRatio);
-	final double ASPECT_TOLERANCE = 0.1;
-	if (sizes == null)
-	    return null;
-	Size optimalSize = null;
-	int resolution = 0;
-
-	for (Size size : sizes) {
-	    double ratio = (double) size.width / size.height;
-	    Log.d(TAG, "######## ratio=" + ratio);
-
-	    int currentResolution = size.width * size.height;
-	    if (Math.abs(ratio - targetRatio) < ASPECT_TOLERANCE
-		    && currentResolution > resolution) {
-		optimalSize = size;
-		resolution = currentResolution;
-	    }
-	}
-
-	return optimalSize;
-    }
-
-    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-	final double ASPECT_TOLERANCE = 0.1;
-	// calculate the ratio of preview display
-	double targetRatio = ((double) w) / ((double) h);
-	// if no supported preview sizes, return null
-	if (sizes == null)
-	    return null;
-
-	Size optimalSize = null;
-	double minDiff = Double.MAX_VALUE;
-
-	// Set target Height based on the
-	int targetHeight = h;
-
-	// Try to find an size match aspect ratio and size
-	for (Size size : sizes) {
-	    double ratio = ((double) size.width) / (double) (size.height);
-
-	    Log.d(TAG, "currentSize width:" + size.width);
-	    Log.d(TAG, "currentSize heigth:" + size.height);
-
-	    if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
-		continue;
-
-	    if (Math.abs(size.height - targetHeight) < minDiff) {
-		optimalSize = size;
-		minDiff = Math.abs(size.height - targetHeight);
-	    }
-	}
-
-	// Cannot find the one match the aspect ratio, ignore the requirement
-	if (optimalSize == null) {
-	    Log.d(TAG, "optimalSize is null");
-	    minDiff = Double.MAX_VALUE;
-	    for (Size size : sizes) {
-		if (Math.abs(size.height - targetHeight) < minDiff) {
-		    optimalSize = size;
-		    minDiff = Math.abs(size.height - targetHeight);
+				&& mSupportedFlashModes
+						.contains(Camera.Parameters.FLASH_MODE_AUTO)) {
+			params.setFocusMode(Camera.Parameters.FLASH_MODE_AUTO);
 		}
-	    }
+		return params;
 	}
-	Log.d(TAG, "optimalSize width:" + optimalSize.width);
-	Log.d(TAG, "optimalSize heigth:" + optimalSize.height);
 
-	return optimalSize;
-    }
+	private Camera.Parameters setFocusModes(Camera.Parameters params) {
+		Log.d(TAG, "setFocusModes is called");
 
-    
-    private class Positon {
-	public int left, right, top, bottom;
-    }
+		if (mAutoFocus
+				.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+			params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+		} else if (mAutoFocus.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+			params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+		}
+		return params;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+
+		Log.d(TAG, "surfaceChanged is called");
+
+		try {
+			mCamera.stopPreview();
+		} catch (Exception e) {
+			Log.e(TAG, " tried to stop a non-existent preview");
+		}
+
+		if (mCamera == null) {
+			Log.e(TAG, " mCamera is null");
+			return;
+		}
+
+		mCamera.setDisplayOrientation(CameraUtil.getRotationAsInt(context));
+		mCamera.startPreview();
+
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+
+		Log.d(TAG, "surfaceDestroyed is called");
+
+		if (mCamera != null) {
+			mCamera.stopPreview();
+		}
+
+	}
+
+	/*
+	 * @Function: get optimal picture size according to camera view angles
+	 */
+	private Size getOptimalPictureSize(List<Size> sizes, double targetRatio) {
+		Log.d(TAG, "OptimalPictureSize targetRatio: " + targetRatio);
+		final double ASPECT_TOLERANCE = 0.1;
+		if (sizes == null)
+			return null;
+		Size optimalSize = null;
+		int resolution = 0;
+
+		for (Size size : sizes) {
+			double ratio = (double) size.width / size.height;
+			Log.d(TAG, "OptimalPictureSize ratio: " + ratio);
+
+			int currentResolution = size.width * size.height;
+			if (Math.abs(ratio - targetRatio) < ASPECT_TOLERANCE
+					&& currentResolution > resolution) {
+				optimalSize = size;
+				resolution = currentResolution;
+			}
+		}
+
+		return optimalSize;
+	}
+
+	private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+		final double ASPECT_TOLERANCE = 0.1;
+		// calculate the ratio of preview display
+		double targetRatio = ((double) w) / ((double) h);
+		// if no supported preview sizes, return null
+		if (sizes == null)
+			return null;
+
+		Size optimalSize = null;
+		double minDiff = Double.MAX_VALUE;
+
+		// Set target Height based on the
+		int targetHeight = h;
+
+		// Try to find an size match aspect ratio and size
+		for (Size size : sizes) {
+			double ratio = ((double) size.width) / (double) (size.height);
+
+			Log.d(TAG, "currentSize width:" + size.width);
+			Log.d(TAG, "currentSize heigth:" + size.height);
+
+			if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+				continue;
+
+			if (Math.abs(size.height - targetHeight) < minDiff) {
+				optimalSize = size;
+				minDiff = Math.abs(size.height - targetHeight);
+			}
+		}
+
+		// Cannot find the one match the aspect ratio, ignore the requirement
+		if (optimalSize == null) {
+			Log.d(TAG, "optimalSize is null");
+			minDiff = Double.MAX_VALUE;
+			for (Size size : sizes) {
+				if (Math.abs(size.height - targetHeight) < minDiff) {
+					optimalSize = size;
+					minDiff = Math.abs(size.height - targetHeight);
+				}
+			}
+		}
+		Log.d(TAG, "optimalSize width:" + optimalSize.width);
+		Log.d(TAG, "optimalSize heigth:" + optimalSize.height);
+
+		return optimalSize;
+	}
+
 }
