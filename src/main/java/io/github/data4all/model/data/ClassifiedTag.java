59a0ecdd9280db33ca78ce0cded47c74dc276605
Steeve <nkemson@tzi.de>
2015-02-12 09:33:27 +0100

    modified the method addSuggestion(). add a new ClassifiedTag (Last Choice), which contains the last TagValues. this Last Choice would be created, when user mapping a object which belong to the same Category as the last object.refs #2838

--- src/main/java/io/github/data4all/model/data/ClassifiedTag.java
+++ src/main/java/io/github/data4all/model/data/ClassifiedTag.java
@@ -16,8 +16,10 @@
 package io.github.data4all.model.data;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This class represents a classified tag. A classified tag is a subclass of
@@ -33,12 +35,12 @@ public class ClassifiedTag extends Tag {
      * stores all classified values for the specific tag
      */
     private List<String> classifiedValues;
-    
+
     /**
      * stores the last classifiedValue Suggestion for the specific tag
      */
-    private List<String> classifiedValuesSuggestion; 
-    
+    private Set<String> classifiedValuesSuggestion;
+
     /**
      * Default constructor.
      * 
@@ -51,15 +53,14 @@ public class ClassifiedTag extends Tag {
             List<String> classifiedValues, int... osmObjects) {
         super(key, type, osmObjects);
         this.classifiedValues = classifiedValues;
-        classifiedValuesSuggestion = new LinkedList<String>();
+        classifiedValuesSuggestion = new LinkedHashSet<String>();
     }
 
     /**
      * @return classifiedValues or classifiedValuesSuggestion
      * 
-     * when the list of suggestion Values is empty ,
-     * then get the classifiedValues
-     * when not then get classifiedValuesSuggestion
+     *         when the list of suggestion Values is empty , then get the
+     *         classifiedValues when not then get classifiedValuesSuggestion
      * 
      */
     public List<String> getClassifiedValues() {
@@ -76,35 +77,53 @@ public class ClassifiedTag extends Tag {
     public void setClassifiedValues(List<String> classifiedValues) {
         this.classifiedValues = classifiedValues;
     }
-     
+
     /**
      * @param classifiedValuesSuggestion
-     * get the list of all classifiedValuesSuggestion 
+     *            get the list of all classifiedValuesSuggestion
      */
-    public List<String> getClassifiedValuesSuggestion() {
+    public Set<String> getClassifiedValuesSuggestion() {
         return classifiedValuesSuggestion;
     }
-    
+
     /**
      * @param suggestion
-     * add a suggestion to the list
-     * or get all elements from list, when the user don't want a suggestion 
+     *    lastChoice is a classifiedTag
+     *    it would created, when user mapping a object which belong to the same Category
+     *    as the last object.
+     *    when lastChoice is null, then create a new classifiedTag and add the suggestion
+     *    when lastChoice is not null then update suggestion.
+     *          
      */
     public void addSuggestion(String suggestion) {
-        classifiedValuesSuggestion.add(suggestion);
-        if (classifiedValuesSuggestion.contains("ALL")) {
-            classifiedValuesSuggestion.remove("ALL");
+
+        // create a tag with this suggestion
+        ClassifiedTag lastChoice = Tags.getLastChoice();
+        if (lastChoice == null) {
+            lastChoice = new ClassifiedTag("Last Choice", getType(),
+                    Arrays.asList(suggestion), getOsmObjects());
+            Tags.tagList.add(lastChoice);
+        } else {
+            lastChoice.setClassifiedValues(Arrays.asList(suggestion));
+            lastChoice.setType(getType());
+            lastChoice.setOsmObjects(getOsmObjects());
+
         }
-        classifiedValuesSuggestion.add("ALL");
-        
+        if ("Last Choice".equalsIgnoreCase(getKey())) {
+            lastChoice.setParentKey(getParentKey());
+        } else {
+            lastChoice.setParentKey(getKey());
+        }
+
     }
-     
+
     /**
      * @return classifiedValues 
-     *                   get all classifiedValues
+     *                     get all classifiedValues
      * 
      */
     public List<String> getAllClassifiedValues() {
         return classifiedValues;
     }
+
 }
