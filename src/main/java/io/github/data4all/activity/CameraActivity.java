d86820ccc6b4857c65dcb6fe2441024b86099f36
Timo Brose <tbrose@fb3.uni-bremen.de>
2015-02-21 18:40:48 +0100

    added more window-requests

--- src/main/java/io/github/data4all/activity/CameraActivity.java
+++ src/main/java/io/github/data4all/activity/CameraActivity.java
@@ -50,7 +50,6 @@ import android.widget.Toast;
  * @version 1.2
  * 
  */
-
 public class CameraActivity extends Activity {
 
 	// Logger Tag
@@ -71,6 +70,9 @@ public class CameraActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
 		setContentView(R.layout.activity_camera);
 
