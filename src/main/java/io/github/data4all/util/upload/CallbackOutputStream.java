9ca5309fb1e6533c5615ad2c7b45a083fcbfeb35
Timo Brose <tbrose@fb3.uni-bremen.de>
2015-03-01 19:05:00 +0100

    Implement a calmdown for the callback refs #3182

--- src/main/java/io/github/data4all/util/upload/CallbackOutputStream.java
+++ src/main/java/io/github/data4all/util/upload/CallbackOutputStream.java
@@ -50,7 +50,9 @@ public final class CallbackOutputStream extends OutputStream {
     @Override
     public void write(int oneByte) throws IOException {
         currentRead++;
-        callback.callback(currentRead);
+        if(currentRead % callback.interval() == 0) {
+            callback.callback(currentRead);
+        }
         outstream.write(oneByte);
     }
 }
