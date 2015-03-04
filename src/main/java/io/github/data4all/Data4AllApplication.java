b3c3b6e2215e7fa36329a41a2ea7c2729550beb1
Timo Brose <tbrose@fb3.uni-bremen.de>
2015-03-04 16:48:44 +0100

    Added first version of ACRA-support

---
+++ src/main/java/io/github/data4all/Data4AllApplication.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014, 2015 Data4All
+ * 
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * <p>Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.data4all;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+import android.app.Application;
+
+/**
+ * @author tbrose
+ *
+ */
+@ReportsCrashes(
+        formUri = "http://www.koronamc.de:5984/acra-data4all/_design/acra-storage/_update/report",
+        reportType = HttpSender.Type.JSON,
+        httpMethod = HttpSender.Method.PUT,
+        formUriBasicAuthLogin = "d4a-reporter",
+        formUriBasicAuthPassword = "ja!doppelkeks",
+        formKey = "", // This is required for backward compatibility but not used
+        customReportContent = {
+                ReportField.APP_VERSION_CODE,
+                ReportField.APP_VERSION_NAME,
+                ReportField.ANDROID_VERSION,
+                ReportField.PACKAGE_NAME,
+                ReportField.REPORT_ID,
+                ReportField.BUILD,
+                ReportField.STACK_TRACE,
+                ReportField.CUSTOM_DATA
+        }
+)
+public class Data4AllApplication extends Application {
+    @Override
+    public final void onCreate() {
+        super.onCreate();
+        ACRA.init(this);
+    }
+}
