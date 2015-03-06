8edffd757dd329404ad3ed86798ab1db53f9770b
Andre Koch <andkoch@fb3.uni-bremen.de>
2015-03-06 09:28:34 +0100

    change all imageButtons to Custom ImageButton refs #2961

--- src/main/java/io/github/data4all/widged/CustomImageButton.java
+++ src/main/java/io/github/data4all/widged/CustomImageButton.java
@@ -7,33 +7,50 @@ import android.util.AttributeSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 
+/**
+ * This method replaces the standard image button with a custom ImageButton .
+ * This includes an animation for onclick effect.
+ * 
+ * @author Andre Koch
+ * @CreationDate 05.03.2015
+ * @LastUpdate 05.03.2015
+ * @version 1.0
+ * 
+ */
 public class CustomImageButton extends ImageButton {
 
+    /**
+     * Default Constructor
+     * @param context
+     */
+    public CustomImageButton(Context context) {
+        super(context);
+    }
 
-	public CustomImageButton(Context context) {
-		super(context);
-	}
+    /**
+     * Default Constructor
+     * @param context
+     */
+    public CustomImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public CustomImageButton(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    /**
+     * Default Constructor
+     * @param context
+     */
+    public CustomImageButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-	public CustomImageButton(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	@Override
-	protected void drawableStateChanged() {
-		Log.d("Button", "isPressed: " + isPressed());
-		if (isPressed()) {
-			startAnimation(AnimationUtils.loadAnimation(getContext(),
-					R.drawable.imagebutton));
+    @Override
+    protected void drawableStateChanged() {
+        if (isPressed()) {
+            startAnimation(AnimationUtils.loadAnimation(getContext(),
+                    R.anim.imagebutton));
+        }
+        super.drawableStateChanged();
 
-		}
-		super.drawableStateChanged();
-
-	}
+    }
 
 }
