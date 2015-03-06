9a3c9154cb6c00cca202dcab762bb4936d451b30
Andre Koch <andkoch@fb3.uni-bremen.de>
2015-03-06 11:49:58 +0100

    change experimental poi button

--- src/main/java/io/github/data4all/widged/CustomImageButton.java
+++ src/main/java/io/github/data4all/widged/CustomImageButton.java
@@ -1,7 +1,6 @@
 package io.github.data4all.widged;
 
 import io.github.data4all.R;
-import io.github.data4all.logger.Log;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.animation.AnimationUtils;
@@ -21,6 +20,7 @@ public class CustomImageButton extends ImageButton {
 
     /**
      * Default Constructor
+     * 
      * @param context
      */
     public CustomImageButton(Context context) {
@@ -29,7 +29,10 @@ public class CustomImageButton extends ImageButton {
 
     /**
      * Default Constructor
+     * 
      * @param context
+     * @param attrs
+     * 
      */
     public CustomImageButton(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -37,7 +40,10 @@ public class CustomImageButton extends ImageButton {
 
     /**
      * Default Constructor
+     * 
      * @param context
+     * @param attrs
+     * @param defStyle
      */
     public CustomImageButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
